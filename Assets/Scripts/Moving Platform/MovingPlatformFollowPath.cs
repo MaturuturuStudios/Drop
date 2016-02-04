348167c6f3bb7091859e06c2b6f9b5b0eab1cba8
Ignacio Astorga Coto <iacfromearth@gmail.com>
2016-02-04 02:21:22 +0100

    Moving Platforms
    Added moving platforms.
    * Created a folder for the Moving Platform's code.
    * Created a prefab for the Moving Platform.
    * Replaced the desired static platforms in the scene with the new moving
    platforms.

---
+++ Assets/Scripts/Moving Platform/MovingPlatformFollowPath.cs
@@ -0,0 +1,77 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class MovingPlatformFollowPath : MonoBehaviour {
+
+	// Custom enumerations
+	public enum FollowType {
+		MoveTowards,
+		Lerp
+	}
+
+	public enum PathType {
+		BackAndForward,
+		Loop
+	}
+
+	// Public atributes
+	public FollowType followType = FollowType.MoveTowards;
+	public PathType pathType = PathType.BackAndForward;
+
+	public MovingPlatformPathDefinition path;
+	public float speed = 10;
+	public float maxDistanceToGoal = 0.1f;
+
+	// Private variables
+	private IEnumerator<Transform> _pathEnumerator;
+
+	public void Start() {
+		// A path is required
+		if (path == null) {
+			Debug.LogError("Path cannot be null!", gameObject);
+			return;
+		}
+
+		// Selects the current path type
+		switch (pathType) {
+			case PathType.BackAndForward:
+				_pathEnumerator = path.getBackAndForwardEumerator();
+				break;
+			case PathType.Loop:
+				_pathEnumerator = path.getLoopEumerator();
+				break;
+			default:
+				Debug.LogError("Unrecognized path type!", gameObject);
+				return;
+		}
+		// Moves the enumerator to the first position and sets the position of the entity
+		_pathEnumerator.MoveNext();
+		if (_pathEnumerator.Current == null)
+			return;
+		transform.position = _pathEnumerator.Current.position;
+	}
+
+	public void Update() {
+		if (_pathEnumerator == null || _pathEnumerator.Current == null)
+			return;
+
+		// Moves the entity using the right function
+		switch (followType) {
+			case FollowType.MoveTowards:
+				transform.position = Vector3.MoveTowards(transform.position, _pathEnumerator.Current.position, speed * Time.deltaTime);
+				break;
+			case FollowType.Lerp:
+				transform.position = Vector3.Lerp(transform.position, _pathEnumerator.Current.position, speed * Time.deltaTime);
+				break;
+			default:
+				return;
+		}
+
+		// Checks if the entity is close enough to the target point
+		float squaredDistance = (transform.position - _pathEnumerator.Current.position).sqrMagnitude;
+		// The squared distance is used becouse a multiplication is cheaper than a square root
+		if (squaredDistance < maxDistanceToGoal * maxDistanceToGoal)
+			_pathEnumerator.MoveNext();
+	}
+}
---
+++ Assets/Scripts/Moving Platform/MovingPlatformPathDefinition.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+using System.Collections.Generic;
+
+public class MovingPlatformPathDefinition : MonoBehaviour {
+
+	// Public atributes
+	public Transform[] points;
+
+    public IEnumerator<Transform> getBackAndForwardEumerator() {
+		// Only one point is required
+        if (points == null || points.Length < 1)
+            yield break;
+
+		bool returning = false;
+        int index = 0;
+        while (true) {
+			// Lazy return
+            yield return points[index];
+
+			// If there is only one point, no further calculations are needed
+			if (points.Length == 1)
+				continue;
+
+			if (index <= 0)
+				returning = false;
+			else if (index >= points.Length - 1)
+				returning = true;
+
+			index += returning ? -1 : 1;
+        }
+	}
+
+	public IEnumerator<Transform> getLoopEumerator() {
+		if (points == null || points.Length < 1)
+			yield break;
+		
+		int index = 0;
+		while (true) {
+			// Lazy return
+			yield return points[index];
+
+			if (index >= points.Length - 1)
+				index = 0;
+			else
+				index++;
+		}
+	}
+
+	public void OnDrawGizmos() {
+        // At least 2 points are needed to draw a line
+        if (points == null || points.Length < 2)
+            return;
+
+        for (int i = 1; i < points.Length; i++)
+            Gizmos.DrawLine(points[i - 1].position, points[i].position);
+    }
+}
