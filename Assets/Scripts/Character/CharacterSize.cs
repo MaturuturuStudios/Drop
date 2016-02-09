dc4b1abd635402b9e44ca0dcfb2f17fb1ce0411e
Dorian Hawkmoon <dorian.cadenas@gmail.com>
2016-02-09 15:42:17 +0100

    Documentation of class CharacterSize.

--- Assets/Scripts/Character/CharacterSize.cs
+++ Assets/Scripts/Character/CharacterSize.cs
@@ -16,24 +16,59 @@ public class CharacterSize : MonoBehaviour {
     public float enlargeSpeed = 5f;
     #endregion
 
-    #region Private Attributes
+    #region Custom private Enumerations
+    /// <summary>
+    /// 
+    /// </summary>
+    private struct InfoAxis {
+        //0 for horizontal
+        //1 for vertical
+        public int axis;
+        public Vector3 offset;
+        public bool block;
+    };
+    #endregion
+
+    #region Variables
+    /// <summary>
+    /// Actual difference with the target size of the character (absolute)
+    /// </summary>
     private float _targettingSize;
     /// <summary>
     /// Growing or decreasing
     /// </summary>
 	private int _shrinkOrEnlarge;
+    /// <summary>
+    /// Actual size of the character
+    /// </summary>
 	private int _size;
+    /// <summary>
+    /// Target size of the character
+    /// </summary>
 	private int _newSize;
     #endregion
 
     #region Private Attributes
+    /// <summary>
+    /// Transform of the character
+    /// </summary>
     private Transform _dropTransform;
+    /// <summary>
+    /// Controller of character (the custom one)
+    /// </summary>
 	private CharacterControllerCustom _dropController;
+    /// <summary>
+    /// Radius of character
+    /// </summary>
     private float _ratioRadius;
     #endregion
 
     #region Methods
-    // Use this for initialization
+    #region Public Methods
+    /// <summary>
+    /// Initialization method.
+    /// The character start with size one
+    /// </summary>
     void Start() {
 		_dropTransform = gameObject.transform;
 		_dropTransform.localScale = Vector3.one;
@@ -45,8 +80,11 @@ public class CharacterSize : MonoBehaviour {
 		SetSize(1);
 	}
 
-	// Update is called once per frame
+	/// <summary>
+    /// Check input and control the size
+    /// </summary>
 	void Update() {
+
         if(Input.GetKeyDown(KeyCode.UpArrow))
 			IncrementSize();
 
@@ -66,14 +104,24 @@ public class CharacterSize : MonoBehaviour {
 		GradualModifySize();
 	}
 
-	public void IncrementSize() {
+    /// <summary>
+    /// Increment the size by one
+    /// </summary>
+    public void IncrementSize() {
 		SetSize(_size + 1);
 	}
 
+    /// <summary>
+    /// Decrement the size by one (with minimum of one)
+    /// </summary>
 	public void DecrementSize() {
 		SetSize(_size - 1);
 	}
 
+    /// <summary>
+    /// Set a size. While changing the size, the character will not move
+    /// </summary>
+    /// <param name="size">New size</param>
 	public void SetSize(int size) {
 		if(size > 0 && this._size != size) {
             //can't move
@@ -85,38 +133,51 @@ public class CharacterSize : MonoBehaviour {
 			float difference = size - _dropTransform.localScale.x;
 			_newSize = size;
 			_targettingSize = Mathf.Abs(difference);
+
 			//positive if I grow up
 			_shrinkOrEnlarge = (difference > 0) ? (int)Mathf.Ceil(difference) :
-				(int)Mathf.Floor(difference);
-			this._size = size;
+				                                (int)Mathf.Floor(difference);
 		}
 	}
 
+    /// <summary>
+    /// Get the actual size
+    /// </summary>
+    /// <returns></returns>
 	public float GetSize() {
 		return _size;
 	}
+    #endregion
 
-	private void GradualModifySize() {
+    #region Private Methods
+    /// <summary>
+    /// Control the gradual change of size
+    /// </summary>
+    private void GradualModifySize() {
+        //if I have pending change...
 		if(_targettingSize > 0) {
-			float previousRadius = _dropTransform.localScale.x * _ratioRadius;
-			float newScale = 0;
-
-			//positive if I grow up
+			//set the size, positive if I grow up
 			float speed = (_shrinkOrEnlarge < 0) ? shrinkSpeed : enlargeSpeed;
-
+            //substract the growth
 			_targettingSize -= Time.deltaTime * speed * Mathf.Abs(_shrinkOrEnlarge);
-			//if finally reached the target size, set the size so we don't have floating remains
-			if(_targettingSize <= 0) {
-				newScale = _size;
+            
+            
+            float newScale = 0;
+            if(_targettingSize <= 0) {
+                //if finally reached the target size, set the size so we don't have floating remains
+                newScale = _size;
 				_targettingSize = 0;
+
 			} else
 				newScale = _dropTransform.localScale.x + Time.deltaTime * speed * _shrinkOrEnlarge;
+            
 
-			float newRadius = newScale * 0.5f;
+            //control if i have space enough and repositioning it
+            Vector3 offset = Vector3.zero;
+            float newRadius = newScale * _ratioRadius;
+            float previousRadius = _dropTransform.localScale.x * _ratioRadius;
 
-			//control if i have space enough and repositioning it
-			Vector3 offset = Vector3.zero;
-			if(!CanSetSize(previousRadius, newRadius, out offset))
+            if(!CanSetSize(previousRadius, newRadius, out offset))
 				SpitDrop();
 			else {
 				//set the new size
@@ -125,7 +186,7 @@ public class CharacterSize : MonoBehaviour {
 				_dropTransform.position += offset;
 			}
 
-            //finished! can move again.
+            //in my size! can move again.
             if(_targettingSize == 0) {
                 CharacterControllerParameters parameters = new CharacterControllerParameters();
                 parameters.movementControl = CharacterControllerParameters.MovementControl.Both;
@@ -134,22 +195,31 @@ public class CharacterSize : MonoBehaviour {
         }
 	}
 
+    /// <summary>
+    /// Check If I have enough space with the actual size
+    /// </summary>
+    /// <param name="previousRadius">The last radius</param>
+    /// <param name="newRadius">The new radius</param>
+    /// <param name="offset">The needed offset for the character for not collide</param>
+    /// <returns></returns>
 	private bool CanSetSize(float previousRadius, float newRadius, out Vector3 offset) {
 		bool canGrowUp = false;
 		offset = Vector3.zero;
 
-		//get the data of the drop
+		//get the position of the character
 		Vector3 position = _dropTransform.position;
 
 		//set the center with the new radius
         float offsetCenter= newRadius - previousRadius;
         position.y += offsetCenter;
 
+        //ask the axis
 		InfoAxis horizontal_axis = checkAxis(0, position, previousRadius, newRadius);
 		InfoAxis vertical_axis = checkAxis(1, position, previousRadius, newRadius);
 
+        //if not blocked...
 		if(!horizontal_axis.block && !vertical_axis.block) {
-			//move the offset...
+			//set the offset and set that I can grow up
 			offset = horizontal_axis.offset + vertical_axis.offset;
             offset.y += offsetCenter;
 			canGrowUp = true;
@@ -158,14 +228,17 @@ public class CharacterSize : MonoBehaviour {
 		return canGrowUp;
 	}
 
+    /// <summary>
+    /// If can't grow up, spit the extra drops and set the maximum size I can
+    /// TODO: spit the extra drops
+    /// </summary>
 	private void SpitDrop() {
-		//TODO spit a drop when I'm growing up and I can't have more
-
 		//final size...
 		float finalScale = _dropTransform.localScale.x;
         //truncate value
 		int finalSize = (int) finalScale;
 
+        //calculate the extra drop I have to spit
 		int numberDropsRemain = _newSize - finalSize;
 		Debug.Log("Spit " + numberDropsRemain + " out");
         
@@ -173,14 +246,12 @@ public class CharacterSize : MonoBehaviour {
         SetSize(finalSize);
 	}
 
-	private struct InfoAxis {
-		//0 for horizontal
-		//1 for vertical
-		public int axis;
-		public Vector3 offset;
-		public bool block;
-	};
-
+    /// <summary>
+    /// Get the vectors of the direction for the RayCast to the given axis and side
+    /// </summary>
+    /// <param name="axis">The axis</param>
+    /// <param name="side">The side (0-1)</param>
+    /// <returns>Three vectors indicating the direction</returns>
 	private Vector3[] getDirectionAxis(int axis, int side) {
 		Vector3[] directionOneSide = new Vector3[3];
 
@@ -217,7 +288,17 @@ public class CharacterSize : MonoBehaviour {
 		return directionOneSide;
 	}
 
+    /// <summary>
+    /// Check an axis.
+    /// </summary>
+    /// <param name="axis">The axis to check</param>
+    /// <param name="position">the center of the character</param>
+    /// <param name="previousRadius">The previous radius of the character</param>
+    /// <param name="radius">The new/actual radius to get the distance to cast</param>
+    /// <returns>Information about the axis. Will include the offset needed to move to avoid collisions.
+    /// If the axis is blocked, the offset is not reliable.</returns>
 	private InfoAxis checkAxis(int axis, Vector3 position, float previousRadius, float radius) {
+        //prepare the information result
 		InfoAxis infoResult;
 		infoResult.axis = axis;
 		infoResult.block = false;
@@ -278,8 +359,10 @@ public class CharacterSize : MonoBehaviour {
 		infoResult.offset = offset;
 		return infoResult;
 	}
+    #endregion
 
-	public void OnCustomCollisionEnter(RaycastHit hit) {
+    #region "Inherited" Methods
+    public void OnCustomCollisionEnter(RaycastHit hit) {
 		// TODO: collision with another drop?
 	}
 
@@ -290,6 +373,6 @@ public class CharacterSize : MonoBehaviour {
 	public void OnCustomCollisionExit(RaycastHit hit) {
 		// TODO: Test method, remove at will
 	}
-
+    #endregion
     #endregion
 }
