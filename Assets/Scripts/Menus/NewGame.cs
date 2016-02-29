1c2bf65a9c95e7c0933de31df54a94951e780bcc
Dorian Hawkmoon <dorian.cadenas@gmail.com>
2016-02-29 13:43:58 +0100

    First version of main menu

---
+++ Assets/Scripts/Menus/NewGame.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NewGame : MonoBehaviour {
+
+	public void LoadSceneNewGame(){
+        SceneManager.LoadScene("Test Scene", LoadSceneMode.Single);
+    }
+}
