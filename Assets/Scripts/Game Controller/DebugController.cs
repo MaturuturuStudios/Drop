642a6ec49fe031b4576d308cf5e002d006b898b4
Ignacio Astorga Coto <iacfromearth@gmail.com>
2016-03-06 00:05:57 +0100

    Debug Mode Complete
    * Added summaries to the DebugController's methods and attributes.
    * Established a default color to the collision color on DebugController.
    * Now DebugController will look for a GameControllerIndependentControl
    component instead of receiving it from a public attribute.
    * Created a prefab from Debug Controller.

--- Assets/Scripts/Game Controller/DebugController.cs
+++ Assets/Scripts/Game Controller/DebugController.cs
@@ -3,47 +3,150 @@ using UnityEngine.UI;
 using System.Text;
 using System.Collections.Generic;
 
+/// <summary>
+/// Class that controls the debug mode. It will read the desired input
+/// commands and show all the important information.
+/// </summary>
 public class DebugController : MonoBehaviour {
 
+	#region Public Attributes
+
+	/// <summary>
+	/// If the debug mode is activated or not. If enabled on the editor,
+	/// the debug mode will start activated.
+	/// </summary>
 	public bool debugMode = false;
 
+	/// <summary>
+	/// If the important information about the character should be visible
+	/// while on debug mode. If enabled on the editor this information 
+	/// will start visible.
+	/// </summary>
 	public bool showInformation = false;
-	public bool showState = false;
-	public bool showParameters = false;
-	
-	public GameControllerIndependentControl _independentControl;
 
+	/// <summary>
+	/// If the character's state information should be visible while on
+	/// debug mode. If enabled on the editor this information will start
+	/// visible.
+	/// </summary>
+	public bool showState = false;
+
+
+	/// <summary>
+	/// If the character's parameters information should be visible while on
+	/// debug mode. If enabled on the editor this information will start
+	/// visible.
+	/// </summary>
+	public bool showParameters = false;
+
+	/// <summary>
+	/// Reference to the debug panel. Configured on the prefab. Should not
+	/// be modified on the editor.
+	/// </summary>
 	public GameObject debugPanel;
 
+	/// <summary>
+	/// Reference to the information text component. Configured on the prefab.
+	/// Should not be modified on the editor.
+	/// </summary>
 	public Text informationText;
+
+	/// <summary>
+	/// Reference to the state text component. Configured on the prefab.
+	/// Should not be modified on the editor.
+	/// </summary>
 	public Text stateText;
+
+	/// <summary>
+	/// Reference to the parameters text component. Configured on the prefab.
+	/// Should not be modified on the editor.
+	/// </summary>
 	public Text parametersText;
 
-	public Color collisionColor;
+	/// <summary>
+	/// The color to tint the colliders the character collides with.
+	/// </summary>
+	public Color collisionColor = Color.red;
 
+	#endregion
+
+	#region Private Attributes
+
+	/// <summary>
+	/// Dictionary containing the original color of each collider tint by
+	/// the debug mode. Use when restoring the color.
+	/// </summary>
 	private Dictionary<Collider, Color> _collidersColors;
 
+	/// <summary>
+	/// Reference to the independent control component from the scene's
+	/// game controller.
+	/// </summary>
+	private GameControllerIndependentControl _independentControl;
+
+	#endregion
+
+	#region Methods
+
+	#region Public GUI Methods
+
+	/// <summary>
+	/// Toggles the debug mode, which displays important information and
+	/// allows some extra operations.
+	/// </summary>
 	public void ToggleDebugMode() {
 		debugMode = !debugMode;
 	}
 
+	/// <summary>
+	/// Hides or unhides the current character's basic information.
+	/// </summary>
 	public void ToggleShowInformation() {
 		showInformation = !showInformation;
 	}
 
+	/// <summary>
+	/// Hides or unhides the current character's state information.
+	/// </summary>
 	public void ToggleShowState() {
 		showState = !showState;
 	}
 
+	/// <summary>
+	/// Hides or unhides the current character's parameters information.
+	/// </summary>
 	public void ToggleShowParameters() {
 		showParameters = !showParameters;
 	}
 
+	/// <summary>
+	/// Destroy the current controlled character.
+	/// </summary>
+	public void DestroyCurrentlyControlledCharacter() {
+		// Destroys the currently controlled character
+		_independentControl.DestroyDrop(_independentControl.currentCharacter);
+	}
+
+	#endregion
+
+	/// <summary>
+	/// Unity's method called at the beginning of the first frame this
+	/// script is active.
+	/// Initializes the script.
+	/// </summary>
 	void Start() {
 		// Initializes the collisions list
 		_collidersColors = new Dictionary<Collider, Color>();
+
+		// Looks for the indpendent controller component
+		_independentControl = FindObjectOfType<GameControllerIndependentControl>();
     }
 	
+	/// <summary>
+	/// Unity's method called at the end of each frame if this script is
+	/// active.
+	/// Handles the input and shows the necessary information.
+	/// </summary>
 	void LateUpdate () {
 		// Toggles debug mode
 		if (Input.GetKeyDown(KeyCode.F1))
@@ -68,14 +171,19 @@ public class DebugController : MonoBehaviour {
 		ShowCharacterState(currentCharacter);
 		ShowCharacterParameters(currentCharacter);
 
-		// Manages the debug input
+		// Manages the events
 		ManageSizeChange(currentCharacter);
 		ManageCharacterCreation();
-
-		// Shows the collisions of the character
 		ShowCharacterCollisions(currentCharacter);
-    }
+	}
 
+	#region Show Information Methods
+
+	/// <summary>
+	/// Updates the GUI text with the basic information of the current
+	/// character.
+	/// </summary>
+	/// <param name="currentCharacter">The current character</param>
 	private void ShowCharacterInformation(GameObject currentCharacter) {
 		// Displays the text
 		informationText.gameObject.SetActive(showInformation);
@@ -118,6 +226,11 @@ public class DebugController : MonoBehaviour {
 		informationText.text = sb.ToString();
     }
 
+	/// <summary>
+	/// Updates the GUI text with the state information of the current
+	/// character.
+	/// </summary>
+	/// <param name="currentCharacter">The current character</param>
 	private void ShowCharacterState(GameObject currentCharacter) {
 		// Displays the text
 		stateText.gameObject.SetActive(showState);
@@ -128,6 +241,11 @@ public class DebugController : MonoBehaviour {
 		stateText.text = currentCharacter.GetComponent<CharacterControllerCustom>().State.ToString();
 	}
 
+	/// <summary>
+	/// Updates the GUI text with the parameters information of the current
+	/// character.
+	/// </summary>
+	/// <param name="currentCharacter">The current character</param>
 	private void ShowCharacterParameters(GameObject currentCharacter) {
 		// Displays the text
 		parametersText.gameObject.SetActive(showParameters);
@@ -138,6 +256,14 @@ public class DebugController : MonoBehaviour {
 		parametersText.text = currentCharacter.GetComponent<CharacterControllerCustom>().Parameters.ToString();
 	}
 
+	#endregion
+
+	#region Handle Events Methods
+
+	/// <summary>
+	/// Reads the input and resizes the current character.
+	/// </summary>
+	/// <param name="currentCharacter">The current character</param>
 	private void ManageSizeChange(GameObject currentCharacter) {
 		// Retrieves the character's size component
 		CharacterSize sizeComponent = currentCharacter.GetComponent<CharacterSize>();
@@ -154,6 +280,9 @@ public class DebugController : MonoBehaviour {
 				sizeComponent.SetSize(i);
 	}
 
+	/// <summary>
+	/// Reads the input and creates new characters.
+	/// </summary>
 	private void ManageCharacterCreation() {
 		// Checks if the creation button has been pressed
 		if (!Input.GetMouseButtonDown(1))
@@ -168,11 +297,11 @@ public class DebugController : MonoBehaviour {
 		newDrop.transform.position = position;
 	}
 
-	public void DestroyCurrentlyControlledCharacter() {
-		// Destroys the currently controlled character
-		_independentControl.DestroyDrop(_independentControl.currentCharacter);
-	}
-
+	/// <summary>
+	/// Changes the color of every game object the current character
+	/// has collided with this frame.
+	/// </summary>
+	/// <param name="currentCharacter">The current character</param>
 	private void ShowCharacterCollisions(GameObject currentCharacter) {
 		// Retrieves the character's controller component
 		CharacterControllerCustom controllerComponent = currentCharacter.GetComponent<CharacterControllerCustom>();
@@ -185,4 +314,8 @@ public class DebugController : MonoBehaviour {
 				material.SetColor("_Color", collisionColor);
 			}
     }
+
+	#endregion
+
+	#endregion
 }
