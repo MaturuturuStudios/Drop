6284d647da9b34536ae9bbf0fb28f68694840885
Roglerto <roglerto@gmail.com>
2016-03-10 14:05:50 +0100

    Jump Musroom editor

--- Assets/Scripts/Jump Moshroom/JumpMushroom.cs
+++ Assets/Scripts/Jump Moshroom/JumpMushroom.cs
@@ -1,8 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class JumpMushroom : MonoBehaviour
-{
+public class JumpMushroom : MonoBehaviour{
    
     private Vector3 FacingDirection;
 
@@ -13,25 +12,28 @@ public class JumpMushroom : MonoBehaviour
     public float Jumpforce = 10;
 
     public bool KeepVerticalSpeed = true;
+    public bool lostcontrol = true;
+    public bool temporaly;
+    public float time;
+    
 
     // Use this for initialization
-    void Start()
-    {
-
+    void Start() {
+        
         FacingDirection = Vector3.up;
         velo.x = 0;
         velo.z = 0;
     }
 
     // Update is called once per frame
-    void Update()
-    {
+    void Update() {
         //FacingDirection = new Vector3(transform.position.x, transform.position.y, 0).normalized;
         FacingDirection = (transform.rotation * FacingDirection);
     }
-    public void OnTriggerEnter(Collider other)
-    {
+    public void OnTriggerEnter(Collider other) {
         CharacterControllerCustomPlayer cccp = other.gameObject.GetComponent<CharacterControllerCustomPlayer>();
+        CharacterControllerCustom parameters = other.gameObject.GetComponent<CharacterControllerCustom>();
+
         if ((cccp != null)) {
             if ((cccp.gameObject.GetComponent<CharacterControllerCustom>().State.IsGrounded == false) || (cccp.gameObject.GetComponent<CharacterControllerCustom>().State.IsFalling == true)){
                 //iff(other.gameObject.GetComponent<CharacterControllerCustomPlayer>().isgrounded==false)
@@ -39,7 +41,7 @@ public class JumpMushroom : MonoBehaviour
                 cccp.gameObject.GetComponent<CharacterControllerCustom>().StopFlying();
 
                velo.y = cccp.GetComponent<CharacterControllerCustom>().Velocity.y;//getvertical
-                velo.y = velo.y * -1;
+               velo.y = velo.y * -1;
 
                 //Debug.Log("LOOOOOOOOOOOOOOOOOOOOL " + velo.y);
 
@@ -54,8 +56,17 @@ public class JumpMushroom : MonoBehaviour
 
                 //cccp.GetComponent<CharacterControllerCustom>().AddForceRelative(FacingDirection*Jumpforce, ForceMode.VelocityChange);
                 //Vector3 finalVelocity = Quaternion.Euler(0, FacingDirection.y, 0) * new Vector3(0, velocity.y, 0);
-                
-                cccp.GetComponent<CharacterControllerCustom>().SendFlying((FacingDirection*velo.y * Jumpforce),false,true,0.5f);
+
+                if (lostcontrol == true) {
+                    if(time==0.0f)
+                        cccp.GetComponent<CharacterControllerCustom>().SendFlying((FacingDirection * velo.y * Jumpforce));
+                    else
+                        cccp.GetComponent<CharacterControllerCustom>().SendFlying((FacingDirection * velo.y * Jumpforce), false, true, time);
+                }
+                if (lostcontrol == false) {
+                    cccp.GetComponent<CharacterControllerCustom>().AddForce(FacingDirection * velo.y * Jumpforce, ForceMode.VelocityChange);
+                    //parameters.Parameters = CharacterControllerParameters.FlyingParameters;
+                }
             }
         }
     }
---
+++ Assets/Scripts/Jump Moshroom/JumpMushroomEditor.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+[CustomEditor(typeof(JumpMushroom))]
+public class JumpMushroomEditor : Editor {
+
+    override public void OnInspectorGUI(){
+
+        /*
+         public float minJump = 1;
+    public float maxJump = 2;
+    public float Jumpforce = 10;
+
+    public bool KeepVerticalSpeed = true;*/
+
+        var myScript = target as JumpMushroom;
+
+        myScript.minJump = EditorGUILayout.FloatField("Min Jump",myScript.minJump);
+        myScript.maxJump = EditorGUILayout.FloatField("Max Jump", myScript.maxJump);
+        myScript.Jumpforce = EditorGUILayout.FloatField("Jump Force", myScript.Jumpforce);
+        myScript.KeepVerticalSpeed = GUILayout.Toggle(myScript.KeepVerticalSpeed, "Keep Vertical Speed ");
+
+        myScript.lostcontrol = GUILayout.Toggle(myScript.lostcontrol, "Lost Control");
+
+        
+
+        if (myScript.KeepVerticalSpeed)
+            myScript.KeepVerticalSpeed = true;
+        if (myScript.KeepVerticalSpeed==false)
+            myScript.KeepVerticalSpeed = false;
+
+
+        if (myScript.lostcontrol) {
+            myScript.temporaly = GUILayout.Toggle(myScript.temporaly, "Lost Control Temporaly");
+            
+        }
+        if (myScript.temporaly) {
+
+            myScript.time = EditorGUILayout.Slider("Time", myScript.time, 0.1f, 1f);
+
+        }
+        if (myScript.temporaly==false)
+        {
+
+            myScript.time = 0.0f ;
+
+        }
+    }
+}
+
