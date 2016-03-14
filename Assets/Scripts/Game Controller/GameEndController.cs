dfdc6ae7db69effc0068483cf3e6d6e6906e4193
toniSoftiberia <alopezs1987+social@gmail.com>
2016-03-14 01:27:51 +0100

    Game end trigger
    *Added End Game prefab
    *Added End Game Script
    *Added canvas to Game Controller

---
+++ Assets/Scripts/Game Controller/GameEndController.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameEndController : MonoBehaviour {
+
+    //On trigger enter control
+    private bool end = false;
+
+    //Editable parameters
+    public float waitTime = 3F;
+    private float _timeElapsed = 0F;
+    public Text gameOverText;
+    public string phrase = "Level Complete!";
+
+    void Start(){
+        //Reset Text
+        gameOverText.text = "";
+    }
+
+	void Update () {
+	    if (end){
+            //Wait time
+            _timeElapsed += Time.deltaTime;
+
+            //Load Menu
+            if (_timeElapsed > waitTime)
+                SceneManager.LoadScene("StartScene");
+        }
+    }
+
+    void OnTriggerEnter(){
+        // Activate counter
+        end = true;
+
+        //Show message
+        gameOverText.text = phrase;
+    }
+
+}
