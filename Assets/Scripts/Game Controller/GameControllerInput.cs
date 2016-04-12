a15e84198f5926de412c212ebafe315d697de520
Ignacio Astorga Coto <iacfromearth@gmail.com>
2016-04-12 13:11:01 +0200

    Shoot Counter verified
    Verified the shoot counter (replaced spaces with tabulations on the
    input script).

--- Assets/Scripts/Game Controller/GameControllerInput.cs
+++ Assets/Scripts/Game Controller/GameControllerInput.cs
@@ -2,55 +2,55 @@
 
 public class GameControllerInput : MonoBehaviour {
 
-    #region Attributes
-    // Internal references
-    private GameControllerIndependentControl _switcher;
-    private MainCameraController _mainCameraController;
+	#region Attributes
+	// Internal references
+	private GameControllerIndependentControl _switcher;
+	private MainCameraController _mainCameraController;
 
-    //menu navigator of the scene
-    private MenuNavigator _ui;
+	//menu navigator of the scene
+	private MenuNavigator _ui;
 
-    /// <summary>
-    /// Control if the trigger is pressed for only call change drop one time
-    /// </summary>
-    private bool _triggerPressed = false;
+	/// <summary>
+	/// Control if the trigger is pressed for only call change drop one time
+	/// </summary>
+	private bool _triggerPressed = false;
 
-    /// <summary>
-    /// Control if the shootCounter is pressed for only call it one time
-    /// </summary>
-    private bool _shootCounterPressed = false;
-    #endregion
+	/// <summary>
+	/// Control if the shootCounter is pressed for only call it one time
+	/// </summary>
+	private bool _shootCounterPressed = false;
+	#endregion
 
-    #region Methods
-    void Start() {
-        // Retrives the independent control component
-        _switcher = GetComponent<GameControllerIndependentControl>();
-        _mainCameraController = GetComponentInChildren<MainCameraController>();
+	#region Methods
+	void Start() {
+		// Retrives the independent control component
+		_switcher = GetComponent<GameControllerIndependentControl>();
+		_mainCameraController = GetComponentInChildren<MainCameraController>();
 
-        _ui = GameObject.FindGameObjectWithTag(Tags.Menus).GetComponent<MenuNavigator>();
-    }
+		_ui = GameObject.FindGameObjectWithTag(Tags.Menus).GetComponent<MenuNavigator>();
+	}
 
 	void Update() {
-        //Start button
-        if (Input.GetButtonDown(Axis.Start))
-            _ui.PauseGame();
+		//Start button
+		if (Input.GetButtonDown(Axis.Start))
+			_ui.PauseGame();
 
-        if (_ui==null || !_ui.IsMenuActive()) {
-            // Retrieves current character's components
-            CharacterControllerCustomPlayer cccp = _switcher.currentCharacter.GetComponent<CharacterControllerCustomPlayer>();
-            CharacterShoot shootComponent = _switcher.currentCharacter.GetComponent<CharacterShoot>();
+		if (_ui==null || !_ui.IsMenuActive()) {
+			// Retrieves current character's components
+			CharacterControllerCustomPlayer cccp = _switcher.currentCharacter.GetComponent<CharacterControllerCustomPlayer>();
+			CharacterShoot shootComponent = _switcher.currentCharacter.GetComponent<CharacterShoot>();
 
-            // Horizontal input
-            float hInput = Input.GetAxis(Axis.Horizontal);
-            cccp.HorizontalInput = hInput;
+			// Horizontal input
+			float hInput = Input.GetAxis(Axis.Horizontal);
+			cccp.HorizontalInput = hInput;
 
-            // Vertical input
-            float vInput = Input.GetAxis(Axis.Vertical);
-            cccp.VerticalInput = vInput;
+			// Vertical input
+			float vInput = Input.GetAxis(Axis.Vertical);
+			cccp.VerticalInput = vInput;
 
-            // Jump input
-            float jumpInput = Input.GetAxis(Axis.Jump);
-            cccp.JumpInput = jumpInput;
+			// Jump input
+			float jumpInput = Input.GetAxis(Axis.Jump);
+			cccp.JumpInput = jumpInput;
 			
 			// Control that triggers are pressed only one time
 			if (!_triggerPressed && Input.GetAxis(Axis.SelectDrop) > 0) {
@@ -66,61 +66,61 @@ public class GameControllerInput : MonoBehaviour {
 
 			// Handles the direct access input
 			if (Input.GetButtonDown(Axis.SelectDrop1))
-                _switcher.SetControl(0);
-            if (Input.GetButtonDown(Axis.SelectDrop2))
-                _switcher.SetControl(1);
-            if (Input.GetButtonDown(Axis.SelectDrop3))
-                _switcher.SetControl(2);
-            if (Input.GetButtonDown(Axis.SelectDrop4))
-                _switcher.SetControl(3);
-            if (Input.GetButtonDown(Axis.SelectDrop5))
-                _switcher.SetControl(4);
+				_switcher.SetControl(0);
+			if (Input.GetButtonDown(Axis.SelectDrop2))
+				_switcher.SetControl(1);
+			if (Input.GetButtonDown(Axis.SelectDrop3))
+				_switcher.SetControl(2);
+			if (Input.GetButtonDown(Axis.SelectDrop4))
+				_switcher.SetControl(3);
+			if (Input.GetButtonDown(Axis.SelectDrop5))
+				_switcher.SetControl(4);
 
-            //Camera looking arround
-            float hLookInput = Input.GetAxis(Axis.CamHorizontal);
-            float vLookInput = Input.GetAxis(Axis.CamVertical);
-            _mainCameraController.LookArround(hLookInput, vLookInput);
+			//Camera looking arround
+			float hLookInput = Input.GetAxis(Axis.CamHorizontal);
+			float vLookInput = Input.GetAxis(Axis.CamVertical);
+			_mainCameraController.LookArround(hLookInput, vLookInput);
 
-            //Handle shoot input To delete
-            if (Input.GetButtonDown(Axis.Action))
-                shootComponent.Shoot();
-            if (Input.GetButtonDown(Axis.ShootMode))
-                shootComponent.Aim();
+			//Handle shoot input To delete
+			if (Input.GetButtonDown(Axis.Action))
+				shootComponent.Shoot();
+			if (Input.GetButtonDown(Axis.ShootMode))
+				shootComponent.Aim();
 
-            // Control that shootCounter is pressed only one time
-            if (!_shootCounterPressed && Input.GetAxis(Axis.ShootCounter) > 0) {
-                shootComponent.IncreaseSize();
-                _shootCounterPressed = true;
-            } else if (!_shootCounterPressed && Input.GetAxis(Axis.ShootCounter) < 0) {
-                shootComponent.DecreaseSize();
-                _shootCounterPressed = true;
-            } else if (Input.GetAxis(Axis.ShootCounter) == 0)
-                _shootCounterPressed = false;
+			// Control that shootCounter is pressed only one time
+			if (!_shootCounterPressed && Input.GetAxis(Axis.ShootCounter) > 0) {
+				shootComponent.IncreaseSize();
+				_shootCounterPressed = true;
+			} else if (!_shootCounterPressed && Input.GetAxis(Axis.ShootCounter) < 0) {
+				shootComponent.DecreaseSize();
+				_shootCounterPressed = true;
+			} else if (Input.GetAxis(Axis.ShootCounter) == 0)
+				_shootCounterPressed = false;
 
-            ///NOT SETTED CONTROLS
-            //Shoot mode pointer inputs
-            if (Input.GetAxis(Axis.LookAtDir) != 0)
-                Debug.Log(Axis.LookAtDir);
+			///NOT SETTED CONTROLS
+			//Shoot mode pointer inputs
+			if (Input.GetAxis(Axis.LookAtDir) != 0)
+				Debug.Log(Axis.LookAtDir);
 
-            //Irrigate action
-            if (Input.GetButtonDown(Axis.Irrigate))
-                Debug.Log(Axis.Irrigate);
+			//Irrigate action
+			if (Input.GetButtonDown(Axis.Irrigate))
+				Debug.Log(Axis.Irrigate);
 
-            //Select button
-            if (Input.GetButtonDown(Axis.Back))
-                Debug.Log(Axis.Back);
+			//Select button
+			if (Input.GetButtonDown(Axis.Back))
+				Debug.Log(Axis.Back);
 
 
-        } else if(_ui!=null){
-            //Select button
-            if (Input.GetButtonDown(Axis.Back)) {
-                _ui.ComeBack();
-                //come back close the menu? unpause the game!!
-                if (!_ui.IsMenuActive()) {
-                    _ui.PauseGame(false);
-                }
-            }
-        }
-    }
-    #endregion
+		} else if(_ui!=null){
+			//Select button
+			if (Input.GetButtonDown(Axis.Back)) {
+				_ui.ComeBack();
+				//come back close the menu? unpause the game!!
+				if (!_ui.IsMenuActive()) {
+					_ui.PauseGame(false);
+				}
+			}
+		}
+	}
+	#endregion
 }
