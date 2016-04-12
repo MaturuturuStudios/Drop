397202865fb0126cb6b7c63122383bfe985d27f7
Ignacio Astorga Coto <iacfromearth@gmail.com>
2016-04-12 14:18:04 +0200

    Final changes on Jump Mushroom
    * Now the JumpMushroom will not send objects flying if it's disabled.
    * Added comments to JumpMushroom.
    * Fixed the custom editor for the Jump Mushroom.

---
+++ Assets/Editor/JumpMushroomEditor.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+/// <summary>
+/// Editor script for the JumpMushroom script.
+/// </summary>
+[CustomEditor(typeof(JumpMushroom))]
+[CanEditMultipleObjects]
+public class JumpMushroomEditor : Editor {
+	
+	/// <summary>
+	/// Draws the information on the editor.
+	/// </summary>
+	public override void OnInspectorGUI() {
+		JumpMushroom myScript = target as JumpMushroom;
+
+		EditorUtility.SetDirty(target);
+
+		myScript.minHeight = EditorGUILayout.FloatField("Min Height", myScript.minHeight);
+		myScript.maxHeight = EditorGUILayout.FloatField("Max Height", myScript.maxHeight);
+
+		myScript.jumpFactor = EditorGUILayout.FloatField("Jump Compensation Factor", myScript.jumpFactor);
+
+		myScript.keepPerpendicularSpeed = GUILayout.Toggle(myScript.keepPerpendicularSpeed, "Keep Perpendicular Speed ");
+
+		myScript.lostControl = GUILayout.Toggle(myScript.lostControl, "Lost Control");
+
+		// If Lost Control is enabled, shows the Temporary option
+		if (myScript.lostControl)
+			myScript.temporary = GUILayout.Toggle(myScript.temporary, "Lost Control Temporary");
+
+		// If Temporary is enabled, shows the Time field
+		if (myScript.temporary)
+			myScript.time = EditorGUILayout.FloatField("Time", myScript.time);
+		else
+			myScript.time = 0.0f;
+	}
+}
+
--- Assets/Scripts/Scenario/Jump Mushroom/JumpMushroom.cs
+++ Assets/Scripts/Scenario/Jump Mushroom/JumpMushroom.cs
@@ -1,62 +1,135 @@
 ﻿using UnityEngine;
 
-public class JumpMushroom : MonoBehaviour{
+/// <summary>
+/// Objects will bounce on this object, keeping their speed in
+/// the oposite direction this object is facing.
+/// </summary>
+	public class JumpMushroom : MonoBehaviour{
 
+	#region Public Attributes
+
+	/// <summary>
+	/// The min height an object will reach after bouncing.
+	/// </summary>
 	public float minHeight = 1.0f;
+
+	/// <summary>
+	/// The max height an object will reach after bouncing.
+	/// </summary>
 	public float maxHeight = 10.0f;
 
+	/// <summary>
+	/// Compensation factor to avoid loose of velocity.
+	/// </summary>
 	public float jumpFactor = 1.015f;
 
+	/// <summary>
+	/// If disabled, objects will be send flying in the same
+	/// direction this object is facin, ignoreing their perpendicular
+	/// velocity
+	/// </summary>
 	public bool keepPerpendicularSpeed = false;
+
+	/// <summary>
+	/// If enabled, the player won't be able to control a bouncing
+	/// character until it touches another collider.
+	/// </summary>
 	public bool lostControl = true;
-	public bool temporaly = true;
+
+	/// <summary>
+	/// If enabled with Lost Control, the player will retake control
+	/// after a short amount of time.
+	/// </summary>
+	public bool temporary = true;
+
+	/// <summary>
+	/// Time until the player retakes control of a bouncing character.
+	/// </summary>
 	public float time = 0.1f;
 
+	#endregion
+
+	#region Private Attributes
+
+	/// <summary>
+	/// Reference to the object's Transform component.
+	/// </summary>
 	private Transform _transform;
 
+	#endregion
+
+	#region Methods
+
+	/// <summary>
+	/// Unity's method called at the beginning of the first frame this
+	/// object is active.
+	/// </summary>
 	void Start() {
 		_transform = transform;
 	}
 	
+	/// <summary>
+	/// Handles collisions with rigidbodys.
+	/// </summary>
+	/// <param name="other">Information about the collision</param>
 	void OnCollisionEnter(Collision other) {
+		if (!enabled)
+			return;
+
 		Rigidbody rb = other.rigidbody;
 		if (rb != null) {
-            float speed = Vector3.Project(other.relativeVelocity, _transform.up).magnitude;
+			// Determines the speed of the object on the facing direction
+			float speed = Vector3.Project(other.relativeVelocity, _transform.up).magnitude;
 
+			// Calculates the speed needed in order to reach the max and min heights
 			float minheightvelocity = Mathf.Sqrt(2 * Physics.gravity.magnitude * minHeight);
 			float maxheightvelocity = Mathf.Sqrt(2 * Physics.gravity.magnitude * maxHeight);
 
+			// Clamps the speed
 			if (speed < minheightvelocity)
 				speed = minheightvelocity;
 			if (speed > maxheightvelocity)
 				speed = maxheightvelocity;
 
-			Vector3 velocity = _transform.up * speed;
+			// Reassigns the speed in the right direction
+			Vector3 velocity = _transform.up * speed * jumpFactor;
 			if (keepPerpendicularSpeed)
-                velocity += Vector3.Project(other.relativeVelocity, _transform.right);
+				velocity += Vector3.Project(other.relativeVelocity, _transform.right);
 			rb.velocity = velocity;
 		}
 	}
 
+	/// <summary>
+	/// HAndles collisions with characters.
+	/// </summary>
+	/// <param name="hit">Information about the collision</param>
 	void OnCustomControllerCollision(ControllerColliderHit hit) {
+		if (!enabled)
+			return;
+
 		CharacterControllerCustom ccc = hit.controller.GetComponent<CharacterControllerCustom>();
 		if (ccc != null) {
-            float speed = Vector3.Project(ccc.BeforeCollisionVelocity, _transform.up).magnitude;
+			// Determines the speed of the object on the facing direction
+			float speed = Vector3.Project(ccc.BeforeCollisionVelocity, _transform.up).magnitude;
 
+			// Calculates the speed needed in order to reach the max and min heights
 			float minheightvelocity = Mathf.Sqrt(2 * ccc.Parameters.Gravity.magnitude * minHeight);
 			float maxheightvelocity = Mathf.Sqrt(2 * ccc.Parameters.Gravity.magnitude * maxHeight);
 
+			// Clamps the speed
 			if (speed < minheightvelocity)
 				speed = minheightvelocity;
 			if (speed > maxheightvelocity)
 				speed = maxheightvelocity;
 
+			// Reassigns the speed in the right direction
 			Vector3 velocity = _transform.up * speed * jumpFactor;
 			if (keepPerpendicularSpeed)
-                velocity += Vector3.Project(ccc.BeforeCollisionVelocity, _transform.right);
+				velocity += Vector3.Project(ccc.BeforeCollisionVelocity, _transform.right);
 
+			// Checks if the player will lose control of the character
 			if (lostControl) {
-				if (!temporaly)
+				if (!temporary)
 					ccc.SendFlying(velocity);
 				else
 					ccc.SendFlying(velocity, false, true, time);
@@ -67,6 +140,10 @@ public class JumpMushroom : MonoBehaviour{
 		}
 	}
 
+	/// <summary>
+	/// Draws the area where the objects will remain bouncing
+	/// on the editor.
+	/// </summary>
 	void OnDrawGizmos() {
 		float height = maxHeight - minHeight;
 		Color color = Color.yellow;
@@ -77,4 +154,6 @@ public class JumpMushroom : MonoBehaviour{
 		Gizmos.matrix = transform.localToWorldMatrix;
 		Gizmos.DrawWireCube(new Vector3(0, minHeight + height / 2, 0f), new Vector3(GetComponent<BoxCollider>().size.x, height, 0.5f));
 	}
+
+	#endregion
 }
