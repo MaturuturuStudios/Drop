2c146fa0b257f875185b4b34eedfdcee1b893cbf
Ignacio Astorga Coto <iacfromearth@gmail.com>
2016-04-18 14:13:22 +0200

    Hide Cursor
    * Created the class IngameMenu which defines a menu shown ingame and
    allows to show and hide the cursor automatically.
    * Created the class GameControllerCursorController for the Game
    Controller. It provides methods to hide and show the cursor.
    * Now the Debug Controller will show the cursor while it is enabled and
    hide it when it isn't.
    * Added the method StopInput to the GameControllerInput component to
    provide a common way to stop the player's input.
    * PauseMenu now uses the StopInput method stated before.
    * PauseMenu's base class is now IngameMenu.
    * Fixed the mouse axis not being properly registered on the Axis class.

---
+++ Assets/Scripts/Menus/IngameMenu.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Base class for an ingame menu. Adds control on
+/// when is the cursor shown or hidden.
+/// </summary>
+public class IngameMenu : MonoBehaviour {
+
+	/// <summary>
+	/// Defines if the cursor should be shown.
+	/// If enabled, the cursor will be shown as soon
+	/// as the player moves it.
+	/// </summary>
+	protected bool EnableCursor {
+		get {
+			return _cursorEnabled;
+		}
+		set {
+			_cursorEnabled = value;
+			if (!_cursorEnabled)
+				_cursorController.HideCursor();
+		}
+	}
+
+	/// <summary>
+	/// Backing field for the EnableCursor property.
+	/// </summary>
+	private bool _cursorEnabled = false;
+
+	/// <summary>
+	/// Reference to the Game Controller's cursor controller
+	/// component.
+	/// </summary>
+	private GameControllerCursorController _cursorController;
+
+	/// <summary>
+	/// Unity's method called after the object is created.
+	/// Retrieves the desired components.
+	/// </summary>
+	protected void Awake() {
+		_cursorController = GameObject.FindGameObjectWithTag(Tags.GameController).GetComponent<GameControllerCursorController>();
+	}
+
+	/// <summary>
+	/// Unity's method called each frame.
+	/// Shows the cursor if the player has moved it.
+	/// </summary>
+	protected void Update() {
+		if (_cursorEnabled)
+			if (Input.GetAxis(Axis.MouseX) != 0 || Input.GetAxis(Axis.MouseY) != 0)
+				_cursorController.ShowCursor();
+	}
+
+	/// <summary>
+	/// Unity's method called when this entity is enabled.
+	/// Enables the cursor.
+	/// </summary>
+	protected void OnEnable() {
+		EnableCursor = true;
+	}
+
+	/// <summary>
+	/// Unity's method called when this entity is disabled.
+	/// Disables the cursor.
+	/// </summary>
+	protected void OnDisable() {
+		EnableCursor = false;
+	}
+}
