4c8babf18f34d4158bb40e719c24d54bbd28a2c1
Ignacio Astorga Coto <iacfromearth@gmail.com>
2016-04-20 03:54:32 +0200

    Trigger Area editor
    * Added a custome ditor for the Trigger Area script.
    * Fixed the toggle fileds on Jump Mushroom's editor not being properly
    shown.

---
+++ Assets/Editor/TriggerAreaEditor.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+/// <summary>
+/// Editor script for the TriggerArea script.
+/// </summary>
+[CustomEditor(typeof(TriggerArea))]
+[CanEditMultipleObjects]
+public class TriggerAreaEditor : Editor {
+	
+	/// <summary>
+	/// Draws the information on the editor.
+	/// </summary>
+	public override void OnInspectorGUI() {
+		TriggerArea myScript = target as TriggerArea;
+
+		EditorUtility.SetDirty(target);
+
+		// Draws the trigger mode field
+		myScript.triggerMode = (TriggerArea.TriggerMode) EditorGUILayout.EnumPopup("Trigger Mode", myScript.triggerMode);
+
+		// If it's a timed switch, draws the time field
+		if (myScript.triggerMode == TriggerArea.TriggerMode.TimedSwitch)
+			myScript.switchTime = EditorGUILayout.FloatField("Switch Time", myScript.switchTime);
+
+		// Draws the auto disable field
+		myScript.autoDisable = EditorGUILayout.Toggle("Auto Disable", myScript.autoDisable);
+
+		// Draws the draw gizmos field
+		myScript.drawGizmos = EditorGUILayout.Toggle("Draw Gizmos", myScript.drawGizmos);
+
+		// Draws the OnEnter methods list
+		SerializedObject serializedScript = new SerializedObject(target);
+		SerializedProperty onEnter = serializedScript.FindProperty("onEnter");
+		EditorGUILayout.PropertyField(onEnter, true);
+
+		// Draws the OnStay methods list, but only if it's not a swtich
+		if (myScript.triggerMode == TriggerArea.TriggerMode.Sensor) {
+			SerializedProperty onStay = serializedScript.FindProperty("onStay");
+			EditorGUILayout.PropertyField(onStay, true);
+		}
+
+		// Draws the OnExit methods list
+		SerializedProperty onExit = serializedScript.FindProperty("onExit");
+		EditorGUILayout.PropertyField(onExit, true);
+	}
+}
+
