0851436fcf0e89b306e3b3369f6d8dd4c30cb38d
Toni Lopez <alopezs1987@gmail.com>
2016-04-24 20:36:08 +0200

    Abstract simple path definition
    * Abstracting simple path definition
    * Added two child classes
    * Addapting PathIterator

---
+++ Assets/Scripts/Follow Path/New Version/BackAndForwardPathDefinition.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+/// <summary>
+/// Defines the path an entity will follow using the MovingPlatformFollowPath script.
+/// </summary>
+public class BackAndForwardPathDefinition : SimplePathDefinition {
+
+    #region Abstract Methods
+
+    /// <summary>
+    /// Advance current position X steps
+    /// </summary>
+    /// <param name="steps">Number of steps to advance</param>
+    /// <returns>Advanced position</returns>
+    public override Transform MoveNext(int steps = 1) {
+
+        if (points.Count < 2) {
+            return GetCurrent();
+        }
+
+        int target = current + steps;
+
+        if (target >= points.Count) {
+            target = (points.Count - (target - points.Count));
+            return null;
+        }
+
+        current = target;
+
+        return points[current];
+    }
+
+
+    /// <summary>
+    /// Goes back current position X steps
+    /// </summary>
+    /// <param name="steps">Number of steps to go back</param>
+    /// <returns>retroceded position</returns>
+    public override Transform MovePrevious(int steps = 1) {
+
+        if (points.Count < 2) {
+            return GetCurrent();
+        }
+
+        int target = current - steps;
+
+        if (target < 0) {
+            target *= -1;
+            return null;
+        }
+
+        current = target;
+
+        return points[current];
+    }
+
+    #endregion
+}
