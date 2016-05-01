6932b328ee32e107bdc52285edf9d19da26eb6b5
Dorian Hawkmoon <dorian.cadenas@gmail.com>
2016-05-01 02:27:50 +0200

    Follow code conventions and create menu's script

---
+++ Assets/Scripts/UI/Menu Options/OptionsHelp.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OptionsHelp : SubOption {
+
+    /// <summary>
+    /// Get the panel of this option (itself)
+    /// </summary>
+    /// <returns></returns>
+    public override GameObject GetPanel() {
+        return gameObject;
+    }
+}
---
+++ Assets/Scripts/UI/Menu Options/OptionsInput.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OptionsInput : SubOption {
+
+    /// <summary>
+    /// Get the panel of this option (itself)
+    /// </summary>
+    /// <returns></returns>
+    public override GameObject GetPanel() {
+        return gameObject;
+    }
+}
