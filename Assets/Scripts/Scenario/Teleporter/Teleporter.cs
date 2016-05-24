e7c7b4cf43aad59de5e683214a9ef2910f12fc7f
Ignacio Astorga Coto <iacfromearth@gmail.com>
2016-05-25 00:12:42 +0200

    Teleporter Prototype
    * Implemented a teleporter prefab prototype. This will help creating
    early puzzles.
    * Two versions were created: One Way teleporter and Two Way teleporter.

---
+++ Assets/Scripts/Scenario/Teleporter/Teleporter.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Quick prototype for the Teleporter plant.
+/// </summary>
+public class Teleporter : ActionPerformer {
+
+	/// <summary>
+	/// The target point the character will be moved to.
+	/// </summary>
+	public Transform target;
+
+	/// <summary>
+	/// Moves the character when the Action button is pressed.
+	/// </summary>
+	/// <param name="character">The currently controlled character</param>
+	protected override void OnAction(GameObject character) {
+		character.transform.position = target.position;
+	}
+
+	/// <summary>
+	/// Unity's method for drawing helpers on the editor.
+	/// </summary>
+	void OnDrawGizmos() {
+		Gizmos.color = Color.magenta;
+		Gizmos.DrawLine(transform.position, target.position);
+	}
+}
