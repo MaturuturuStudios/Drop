6907e5b2c8296b6f272e25c2d49aad318de29026
Roglerto <roglerto@gmail.com>
2016-05-26 13:26:54 +0200

    Robert Disaster level

---
+++ Assets/Scripts/Scenario/Triggers/DoorTriggerDown.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DoorTriggerDown : MonoBehaviour {
+
+
+    private bool isopen = false;
+    /// <summary>
+    /// Opens the door.
+    /// </summary>
+    public void Open()
+    {
+        if (!isopen)
+        {
+            transform.Translate(new Vector3(0, -20, 0));
+            isopen = true;
+        }
+    }
+    /// <summary>
+    /// Closes the door.
+    /// </summary>
+    public void Close()
+    {
+        if (isopen)
+        {
+            transform.Translate(new Vector3(0, 20, 0));
+            isopen = false;
+        }
+    }
+}
