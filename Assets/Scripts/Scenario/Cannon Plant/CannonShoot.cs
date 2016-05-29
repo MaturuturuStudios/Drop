ae9ea5c1d78b4f2c751a2d2c670fad80cb1ed26d
Ignacio Astorga Coto <iacfromearth@gmail.com>
2016-05-30 01:36:39 +0200

    Fixed Cannon Plant prefab
    * Renamed target and source game objects.
    * Modified the mesh to be more informative.
    * Removed the renderer from the target object. It now relies on a Gizmo.
    * Fixed the trajectory hitting triggers and non-scene colliders.
    * Fixed the trajectory's gizmo passing through the level.

--- Assets/Scripts/Scenario/Cannon Plant/CannonShoot.cs
+++ Assets/Scripts/Scenario/Cannon Plant/CannonShoot.cs
@@ -7,324 +7,303 @@ using System.Collections.Generic;
 /// </summary>
 public class CannonShoot : ActionPerformer
 {
-    #region Private Attributes
+	#region Private Attributes
 
-    /// <summary>
-    /// This is to draw the animation of the particle that move throught the trajectory 
-    /// </summary>
-    private float journeyLength;
-    private float faction_of_path_traveled;
+	/// <summary>
+	/// This is to draw the animation of the particle that move throught the trajectory 
+	/// </summary>
+	private float journeyLength;
+	private float faction_of_path_traveled;
 
-    /// <summary>
-    /// This is the arrays of the trajectory points
-    /// </summary>
-    private List<GameObject> trajectoryPoints;
+	/// <summary>
+	/// This is the arrays of the trajectory points
+	/// </summary>
+	private List<GameObject> trajectoryPoints;
 
-    /// <summary>
-    /// These are the scripts objects
-    /// </summary>
-    private CharacterControllerCustom cc;
+	/// <summary>
+	/// These are the scripts objects
+	/// </summary>
+	private CharacterControllerCustom cc;
    
 
-    private Vector3 oldvector;
+	private Vector3 oldvector;
 
-    /// <summary>
-    /// Vector which contain the information that we need to shoot a drop in the sendflying method
-    /// </summary>
-    private Vector3  pVelocity;
+	/// <summary>
+	/// Vector which contain the information that we need to shoot a drop in the sendflying method
+	/// </summary>
+	private Vector3  pVelocity;
 
 
-    /// <summary>
-    /// Ray cast to know where the trajectory points are hitting
-    /// </summary>
-    private RaycastHit hit;
-    /// <summary>
-    /// Vector auxiliar to keep data
-    /// </summary>
-    private Vector3 fwd;
+	/// <summary>
+	/// Ray cast to know where the trajectory points are hitting
+	/// </summary>
+	private RaycastHit hit;
+	/// <summary>
+	/// Vector auxiliar to keep data
+	/// </summary>
+	private Vector3 fwd;
 
-    /// <summary>
-    /// Variable to keep data
-    /// </summary>
-    private float velocity = 1;
+	/// <summary>
+	/// Variable to keep data
+	/// </summary>
+	private float velocity = 1;
 
-    /// <summary>
-    /// Boolean to know if the raycast hitted something
-    /// </summary>
-    private bool colisiondetected = false;
+	/// <summary>
+	/// Boolean to know if the raycast hitted something
+	/// </summary>
+	private bool colisiondetected = false;
 
-    #endregion
+	#endregion
 
-    #region Public Attributes
+	#region Public Attributes
 
-    /// <summary>
-    /// Prefab of the trajectory points
-    /// </summary>
-    public GameObject TrajectoryPointPrefeb;
+	/// <summary>
+	/// Prefab of the trajectory points
+	/// </summary>
+	public GameObject TrajectoryPointPrefeb;
 
-    /// <summary>
-    /// Number of trajectoyr points we will have
-    /// </summary>
-    public int numOfTrajectoryPoints = 100;
+	/// <summary>
+	/// Number of trajectoyr points we will have
+	/// </summary>
+	public int numOfTrajectoryPoints = 100;
 
-    /// <summary>
-    /// Angle of the trajectory that we will changing with the input axis
-    /// </summary>
-    public float angle=45;
+	/// <summary>
+	/// Layers the trajectory will collide with.
+	/// </summary>
+	public LayerMask layerMask;
 
-    /// <summary>
-    ///  the power that the drop will be shooted
-    /// </summary>
+	/// <summary>
+	/// Angle of the trajectory that we will changing with the input axis
+	/// </summary>
+	public float angle=45;
 
-    public GameObject target;
+	/// <summary>
+	///  the power that the drop will be shooted
+	/// </summary>
 
-    #endregion
+	public GameObject target;
 
-    #region Methods
+	#endregion
 
-    /// <summary>
+	#region Methods
+
+	/// <summary>
 	/// Unity's method called when the entity is created.
 	/// Recovers the desired componentes of the entity.
 	/// </summary>
-    void Start()
-    {
+	void Start()
+	{
+		Vector3 start;
+		start.x = transform.position.x + 5;
+		start.y = transform.position.y + 5;
+		start.z = 0;
 
-        target.GetComponent<Renderer>().enabled = false;
+		cc = GameObject.FindGameObjectWithTag(Tags.Player)
+								.GetComponent<CharacterControllerCustom>();
 
-        Vector3 start;
-        start.x = transform.position.x + 5;
-        start.y = transform.position.y + 5;
-        start.z = 0;
+		trajectoryPoints = new List<GameObject>();
 
-        cc = GameObject.FindGameObjectWithTag(Tags.Player)
-                                .GetComponent<CharacterControllerCustom>();
-
-        trajectoryPoints = new List<GameObject>();
-
-        for (int i = 0; i < numOfTrajectoryPoints; i++)
-        {
-            GameObject dot = (GameObject)Instantiate(TrajectoryPointPrefeb);
-            dot.GetComponent<Renderer>().enabled = false;
-            //dot.tag = ("Trajectory"+i);
-            dot.transform.parent = transform;
-            trajectoryPoints.Insert(i, dot);
-        }
+		for (int i = 0; i < numOfTrajectoryPoints; i++)
+		{
+			GameObject dot = (GameObject)Instantiate(TrajectoryPointPrefeb);
+			dot.GetComponent<Renderer>().enabled = false;
+			//dot.tag = ("Trajectory"+i);
+			dot.transform.parent = transform;
+			trajectoryPoints.Insert(i, dot);
+		}
 
         
 
-    }
+	}
 
-    /// <summary>
-    /// Unity's method called each frame.
-    /// </summary>
-    /// 
+	/// <summary>
+	/// Unity's method called each frame.
+	/// </summary>
+	/// 
 
-    public void Update()
-    {
+	public void Update()
+	{
 
-        transform.eulerAngles = new Vector3(0, 0, angle); //this is to face in the direction you are aming
-        Vector3 pos = this.transform.position;            // float speed = Mathf.Sqrt((power) * ccc.Parameters.Gravity.magnitude);
+		transform.eulerAngles = new Vector3(0, 0, angle); //this is to face in the direction you are aming
+		Vector3 pos = this.transform.position;            // float speed = Mathf.Sqrt((power) * ccc.Parameters.Gravity.magnitude);
 
-        velocity = (-cc.Parameters.Gravity.magnitude * (target.transform.position.x - transform.position.x) * (target.transform.position.x - transform.position.x)) / (2 * Mathf.Cos(angle * Mathf.Deg2Rad) * Mathf.Cos(angle * Mathf.Deg2Rad) * ((target.transform.position.y - transform.position.y) - Mathf.Tan(angle * Mathf.Deg2Rad) * (target.transform.position.x - transform.position.x)));
+		velocity = (-cc.Parameters.Gravity.magnitude * (target.transform.position.x - transform.position.x) * (target.transform.position.x - transform.position.x)) / (2 * Mathf.Cos(angle * Mathf.Deg2Rad) * Mathf.Cos(angle * Mathf.Deg2Rad) * ((target.transform.position.y - transform.position.y) - Mathf.Tan(angle * Mathf.Deg2Rad) * (target.transform.position.x - transform.position.x)));
 
-        velocity = Mathf.Sqrt(velocity);
+		velocity = Mathf.Sqrt(velocity);
 
-        setTrajectoryPoints(pos, angle, velocity);
-        setvisibility();
+		setTrajectoryPoints(pos, angle, velocity);
+		setvisibility();
 
 
-    }
-    protected override void OnAction(GameObject character)
-    {
-        CharacterControllerCustom ccc = character.GetComponent<CharacterControllerCustom>();
+	}
+	protected override void OnAction(GameObject character)
+	{
+		CharacterControllerCustom ccc = character.GetComponent<CharacterControllerCustom>();
                 
-                ccc.transform.position = this.transform.position;
-                ccc.Stop();
-                ccc.SendFlying(GetpVelocity());
-               // Debug.Log(" angle " + transform.eulerAngles);                  
+				ccc.transform.position = this.transform.position;
+				ccc.Stop();
+				ccc.SendFlying(GetpVelocity());
+				// Debug.Log(" angle " + transform.eulerAngles);                  
 
-    }
-
-
-    /// <summary>
-    /// Method to change the angle and the power of the plant when drop touch the trigger
-    /// </summary>
-    public void Changeangle(float vangle)
-    {
-        angle = vangle;
-
-    }
+	}
 
 
+	/// <summary>
+	/// Method to change the angle and the power of the plant when drop touch the trigger
+	/// </summary>
+	public void Changeangle(float vangle)
+	{
+		angle = vangle;
 
-    /// <summary>
-    /// This fuctions delete the trajectory
-    /// </summary>
-    public void QuitTrajectory()
-    {
-        for (int i = 0; i < numOfTrajectoryPoints; i++)
-        {
-            trajectoryPoints[i].GetComponent<Renderer>().enabled = false;
-        }
+	}
+
+
+
+	/// <summary>
+	/// This fuctions delete the trajectory
+	/// </summary>
+	public void QuitTrajectory()
+	{
+		for (int i = 0; i < numOfTrajectoryPoints; i++)
+		{
+			trajectoryPoints[i].GetComponent<Renderer>().enabled = false;
+		}
         
-    }
+	}
 
-    ///  <summary>
-    /// This fuctions return the shoot vector for the shoot script
-    /// </summary>
-    public Vector3 GetpVelocity()
-    {
+	///  <summary>
+	/// This fuctions return the shoot vector for the shoot script
+	/// </summary>
+	public Vector3 GetpVelocity()
+	{
 
-        Vector2 pVelocity = new Vector3(Mathf.Cos(angle * Mathf.Deg2Rad) * velocity, Mathf.Sin(angle * Mathf.Deg2Rad) * velocity, 0);
+		Vector2 pVelocity = new Vector3(Mathf.Cos(angle * Mathf.Deg2Rad) * velocity, Mathf.Sin(angle * Mathf.Deg2Rad) * velocity, 0);
         
-        return pVelocity;
-    }
+		return pVelocity;
+	}
 
-    ///  <summary>
-    /// This fuctions calculate the points of the trajectory and the shoot vector which is pVelocity
-    /// </summary>
-    void setTrajectoryPoints(Vector3 pStartPosition, float angle, float velocity)
-    {
+	///  <summary>
+	/// This fuctions calculate the points of the trajectory and the shoot vector which is pVelocity
+	/// </summary>
+	void setTrajectoryPoints(Vector3 pStartPosition, float angle, float velocity)
+	{
 
-        float fTime = 0;
+		float fTime = 0;
 
-        fTime += 0.1f;
-        for (int i = 0; i < numOfTrajectoryPoints; i++)
-        {
-            float dx = velocity * fTime * Mathf.Cos(angle * Mathf.Deg2Rad);
-            float dy = velocity * fTime * Mathf.Sin(angle * Mathf.Deg2Rad) - (cc.Parameters.Gravity.magnitude * fTime * fTime / 2.0f);
-            Vector3 pos = new Vector3(pStartPosition.x + dx, pStartPosition.y + dy, 0);
-            trajectoryPoints[i].transform.position = Vector3.MoveTowards(trajectoryPoints[i].transform.position, pos, 100);
-            trajectoryPoints[i].GetComponent<Renderer>().enabled = true;
-            trajectoryPoints[i].transform.eulerAngles = new Vector3(0, 0, Mathf.Atan2(target.transform.position.y - (cc.Parameters.Gravity.magnitude) * fTime, target.transform.position.x) * Mathf.Rad2Deg);
-            fTime += 0.1f;
-        }
+		fTime += 0.1f;
+		for (int i = 0; i < numOfTrajectoryPoints; i++)
+		{
+			float dx = velocity * fTime * Mathf.Cos(angle * Mathf.Deg2Rad);
+			float dy = velocity * fTime * Mathf.Sin(angle * Mathf.Deg2Rad) - (cc.Parameters.Gravity.magnitude * fTime * fTime / 2.0f);
+			Vector3 pos = new Vector3(pStartPosition.x + dx, pStartPosition.y + dy, 0);
+			trajectoryPoints[i].transform.position = Vector3.MoveTowards(trajectoryPoints[i].transform.position, pos, 100);
+			trajectoryPoints[i].GetComponent<Renderer>().enabled = true;
+			trajectoryPoints[i].transform.eulerAngles = new Vector3(0, 0, Mathf.Atan2(target.transform.position.y - (cc.Parameters.Gravity.magnitude) * fTime, target.transform.position.x) * Mathf.Rad2Deg);
+			fTime += 0.1f;
+		}
 
-    }
+	}
 
-    ///  <summary>
-    /// This fuctions draw the particle trip along the trajectory
-    /// </summary>
-   /* public void Example()
-    {
-         //Debug.Log("FINAL " + finalWaypoint);
-            if (nextWaypoint > finalWaypoint)
-            {
-                nextWaypoint = 1;
-                lastWaypoint = 0;
-            }
+	///  <summary>
+	/// This fuctions draw the particle trip along the trajectory
+	/// </summary>
+	/* public void Example()
+	{
+			//Debug.Log("FINAL " + finalWaypoint);
+			if (nextWaypoint > finalWaypoint)
+			{
+				nextWaypoint = 1;
+				lastWaypoint = 0;
+			}
 
-            Vector3 fullPath = trajectoryPoints[nextWaypoint].transform.position - trajectoryPoints[lastWaypoint].transform.position; //defines the path between lastWaypoint and nextWaypoint as a Vector3
-            faction_of_path_traveled += particletrajectoryspeed * Time.deltaTime; //animate along the path
-            if (faction_of_path_traveled > 1) //move to next waypoint
-            {
-                lastWaypoint++; nextWaypoint++;
+			Vector3 fullPath = trajectoryPoints[nextWaypoint].transform.position - trajectoryPoints[lastWaypoint].transform.position; //defines the path between lastWaypoint and nextWaypoint as a Vector3
+			faction_of_path_traveled += particletrajectoryspeed * Time.deltaTime; //animate along the path
+			if (faction_of_path_traveled > 1) //move to next waypoint
+			{
+				lastWaypoint++; nextWaypoint++;
 
-                faction_of_path_traveled = 0;
+				faction_of_path_traveled = 0;
 
-                return;
-            }
+				return;
+			}
 
-            sphere.transform.position = (fullPath * faction_of_path_traveled) + trajectoryPoints[lastWaypoint].transform.position;
+			sphere.transform.position = (fullPath * faction_of_path_traveled) + trajectoryPoints[lastWaypoint].transform.position;
         
-    }*/
+	}*/
 
-    ///  <summary>
-    /// This fuction draw the trajectory prefab depending on the colisions with their raycast
-    /// </summary>
-    public void setvisibility()
-    {
-        float dis = 0;
-        int j=0;
+	///  <summary>
+	/// This fuction draw the trajectory prefab depending on the colisions with their raycast
+	/// </summary>
+	public void setvisibility()
+	{
+		float dis = 0;
+		int j=0;
 
-        for (int i = 0; i < numOfTrajectoryPoints - 1 && !colisiondetected; i++)
-        {
+		for (int i = 0; i < numOfTrajectoryPoints - 1 && !colisiondetected; i++)
+		{
 
-            trajectoryPoints[i].GetComponent<Renderer>().enabled = true;
-            fwd = trajectoryPoints[i + 1].transform.position - trajectoryPoints[i].transform.position;
+			trajectoryPoints[i].GetComponent<Renderer>().enabled = true;
+			fwd = trajectoryPoints[i + 1].transform.position - trajectoryPoints[i].transform.position;
 
-            dis = fwd.magnitude;
+			dis = fwd.magnitude;
 
-            if ((Physics.Raycast(trajectoryPoints[i].transform.position, fwd, dis)))
-            {
-                colisiondetected = true;
+			if ((Physics.Raycast(trajectoryPoints[i].transform.position, fwd, dis, layerMask)))
+			{
+				colisiondetected = true;
 
 
-                for (j = i + 1; j < numOfTrajectoryPoints - 1; j++)
-                {
-                    trajectoryPoints[j].GetComponent<Renderer>().enabled = false;
+				for (j = i + 1; j < numOfTrajectoryPoints - 1; j++)
+				{
+					trajectoryPoints[j].GetComponent<Renderer>().enabled = false;
 
-                }
-                trajectoryPoints[numOfTrajectoryPoints - 1].GetComponent<Renderer>().enabled = false;
+				}
+				trajectoryPoints[numOfTrajectoryPoints - 1].GetComponent<Renderer>().enabled = false;
                 
-            }
+			}
 
-            Debug.DrawRay(trajectoryPoints[i].transform.position, fwd, Color.green);
+			Debug.DrawRay(trajectoryPoints[i].transform.position, fwd, Color.green);
            
 
-        }
-        colisiondetected = false;
+		}
+		colisiondetected = false;
 
-    }
+	}
 
-    public void OnDrawGizmos()
-    {
-        if (!Application.isPlaying)
-        {
-            
-            RaycastHit hitpoint;
+	public void OnDrawGizmos()
+	{
+		if (!Application.isPlaying) {
+			transform.eulerAngles = new Vector3(0, 0, angle); //this is to face in the direction you are aming
 
-            List<Vector3> puntos = new List<Vector3>();
+			RaycastHit hitpoint;
         
-            velocity = (-25* (target.transform.position.x - transform.position.x) * (target.transform.position.x - transform.position.x)) / (2 * Mathf.Cos(angle * Mathf.Deg2Rad) * Mathf.Cos(angle * Mathf.Deg2Rad) * ((target.transform.position.y - transform.position.y)- Mathf.Tan(angle * Mathf.Deg2Rad) * (target.transform.position.x - transform.position.x)));
+			velocity = (-25* (target.transform.position.x - transform.position.x) * (target.transform.position.x - transform.position.x)) / (2 * Mathf.Cos(angle * Mathf.Deg2Rad) * Mathf.Cos(angle * Mathf.Deg2Rad) * ((target.transform.position.y - transform.position.y)- Mathf.Tan(angle * Mathf.Deg2Rad) * (target.transform.position.x - transform.position.x)));
  
-            velocity= Mathf.Sqrt(velocity);
+			velocity = Mathf.Sqrt(velocity);
 
-            float fTime = 0;
+			float fTime = 0;
 
-            fTime += 0.1f;
-            for (int i = 0; i < numOfTrajectoryPoints; i++)
-            {
-                float dx = velocity * fTime * Mathf.Cos(angle * Mathf.Deg2Rad);
-               
-                float dy = velocity * fTime * Mathf.Sin(angle * Mathf.Deg2Rad) - ((25) * fTime * fTime / 2.0f);
-               
-                Vector3 posi = new Vector3(transform.position.x + dx, transform.position.y + dy, 0);
-                puntos.Insert(i, posi);
-                fTime += 0.1f;
+			Vector3 lastPoint = transform.position;
 
-                if (i > 0)
-                {
-                    Vector3 f = puntos[i-1] - puntos[i];
-                    if ((Physics.Raycast(puntos[i], f, out hitpoint, f.magnitude)))
-                    {
-                        i = numOfTrajectoryPoints;
-                       // Vector3 hitting = hitpoint.point;
-                       // float displacement = -1;                       
-                       // Gizmos.color = Color.red;
-                        //Gizmos.DrawSphere(hitting + hitpoint.normal*displacement , 1);
+			Gizmos.color = Color.green;
+			for (int i = 0; i < numOfTrajectoryPoints; i++) {
+				fTime += 0.1f;
+				float dx = velocity * fTime * Mathf.Cos(angle * Mathf.Deg2Rad);
+				float dy = velocity * fTime * Mathf.Sin(angle * Mathf.Deg2Rad) - ((25) * fTime * fTime / 2.0f);
+				Vector3 posi = transform.position + new Vector3(dx, dy, 0);
+				Vector3 d = posi - lastPoint;
 
-                    }
-                    else
-                    {
-                        //Vector3 fi = puntos[i] - puntos[i+1];
-                        Gizmos.color = Color.green;
-                        Gizmos.DrawRay(puntos[i], f);
-                    }
-                }
-                if(i==0)
-                {
-                    Vector3 f = transform.position-puntos[i];
-                    Gizmos.color = Color.green;
-                    Gizmos.DrawRay(puntos[i], f);
-                }
+				if (Physics.Raycast(lastPoint, d, out hitpoint, d.magnitude, layerMask)) {
+					Gizmos.DrawLine(lastPoint, hitpoint.point);
+					break;
+				}
+				else
+				{
+					Gizmos.DrawLine(lastPoint, posi);
+					lastPoint = posi;
+				}
+			}
+		}
+	}
 
-            }
-
-
-        }
-
-    }
-
-    #endregion
+	#endregion
 }
