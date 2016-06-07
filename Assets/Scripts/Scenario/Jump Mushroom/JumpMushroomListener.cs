c710034beae19e2a3102208c364eb951e3e580ed
Ignacio Astorga Coto <iacfromearth@gmail.com>
2016-06-08 00:19:13 +0200

    Jump Mushroom Sound
    * Added JumpMushroomListener interface for the JumpMushroom's events.
    * Added a sound to JumpMushrooms whenever an object bounces on it.
    * Fixed the mushrooms on the main scene not using the proper prefab.

---
+++ Assets/Scripts/Scenario/Jump Mushroom/JumpMushroomListener.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Interface for listeners to a JumpMushroom component.
+/// </summary>
+public interface JumpMushroomListener {
+
+	/// <summary>
+	/// Method called when an object bounces on the mushroom
+	/// </summary>
+	/// <param name="mushroom">The mushroom the object is bouncing on</param>
+	/// <param name="bouncingCharacter">The object bouncing on the mushroom</param>
+	/// <param name="bounceVelocity">The velocity of the bounce</param>
+	void OnBounce(JumpMushroom mushroom, GameObject bouncingCharacter, Vector3 bounceVelocity);
+}
---
+++ Assets/Scripts/Scenario/Jump Mushroom/JumpMushroomSound.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Listener to the mushroom's events that plays the bouncing sound.
+/// </summary>
+[RequireComponent(typeof(AudioSource))]
+[RequireComponent(typeof(JumpMushroom))]
+public class JumpMushroomSound : MonoBehaviour, JumpMushroomListener {
+
+	/// <summary>
+	/// If enabled, the pitch of the sound will be modified by the
+	/// bouncing character's size;
+	/// </summary>
+	public bool modifyPitch = true;
+
+	/// <summary>
+	/// Reference to this entity's AudioSource component.
+	/// </summary>
+	private AudioSource _audioSource;
+
+	void Awake() {
+		// Retireves the desired components
+		_audioSource = GetComponent<AudioSource>();
+	}
+
+	void Start() {
+		// Subscribes itself to thepublisher
+		GetComponent<JumpMushroom>().AddListener(this);
+	}
+
+	public void OnBounce(JumpMushroom mushroom, GameObject bouncingCharacter, Vector3 bounceVelocity) {
+		if (modifyPitch) {
+			CharacterSize characterSize = bouncingCharacter.GetComponent<CharacterSize>();
+			_audioSource.pitch = 1.0f;
+			if (characterSize != null)
+				_audioSource.pitch /= Mathf.Sqrt(characterSize.GetSize());
+		}
+		_audioSource.Play();
+	}
+}
