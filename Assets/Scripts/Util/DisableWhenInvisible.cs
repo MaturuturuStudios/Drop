6cb4815c42c18fb8ae9ccfd88f42d16ca9c4ae5c
Ignacio Astorga Coto <iacfromearth@gmail.com>
2016-06-08 03:31:46 +0200

    DisableWhenInvisible
    * Added the component DisableWhenInvisible which will disable the
    specified components while the attached renderer is not visible.
    * Character's deformation and eye atachment are now disabled when the
    character is out of camera.

---
+++ Assets/Scripts/Util/DisableWhenInvisible.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Disables a set of components whenever the entity is not
+/// visible by the camera. Reenables if it becomes visible
+/// again.
+/// </summary>
+[RequireComponent(typeof(Renderer))]
+public class DisableWhenInvisible : MonoBehaviour {
+
+	/// <summary>
+	/// Set of components to disable.
+	/// </summary>
+	public MonoBehaviour[] components;
+
+	void OnBecameVisible() {
+		foreach (MonoBehaviour component in components)
+			component.enabled = true;
+	}
+
+	void OnBecameInvisible() {
+		foreach (MonoBehaviour component in components)
+			component.enabled = false;
+	}
+}
