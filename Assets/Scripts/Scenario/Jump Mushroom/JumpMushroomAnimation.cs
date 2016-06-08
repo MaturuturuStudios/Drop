77b32bc7e18ffb26b8eebb4054c8bed7fe2940c7
Ignacio Astorga Coto <iacfromearth@gmail.com>
2016-06-08 02:13:13 +0200

    Jump Mushroom Animation
    * Added a listener to JumpMushroom which plays an animation on the
    object.
    * Created two animations on the mushrooms assests, one for idle and
    another one for bouncing.
    * Moved and reworked the mushrooms' animators to fit the new behaviour.
    Many of them just use and Animator Overrider from the original one.

---
+++ Assets/Scripts/Scenario/Jump Mushroom/JumpMushroomAnimation.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Listener to the mushroom's events that creates plays an animation on the mushroom.
+/// </summary>
+[RequireComponent(typeof(JumpMushroom))]
+public class JumpMushroomAnimation : MonoBehaviour, JumpMushroomListener {
+
+	/// <summary>
+	/// The effect created when something bounces on it.
+	/// </summary>
+	private Animator _animator;
+
+	/// <summary>
+	/// The key string to the bounce trigger on the animator.
+	/// </summary>
+	private static readonly string ANIMATOR_TRIGGER_STRING = "Bounce";
+
+	void Awake() {
+		// Retrieves the desired components
+		_animator = GetComponentInChildren<Animator>();
+	}
+
+	void Start() {
+		// Subscribes itself to thepublisher
+		GetComponent<JumpMushroom>().AddListener(this);
+	}
+
+	public void OnBounce(JumpMushroom mushroom, GameObject bouncingCharacter, Vector3 bounceVelocity, Vector3 collisionPoint, Vector3 collisionNormal) {
+		if (_animator != null)
+			_animator.SetTrigger(ANIMATOR_TRIGGER_STRING);
+	}
+}
