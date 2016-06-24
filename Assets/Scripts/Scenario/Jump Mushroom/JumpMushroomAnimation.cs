b4c3e7072a61557ab4a39ed92275df93b2f2770b
Ignacio Astorga Coto <iacfromearth@gmail.com>
2016-06-24 17:26:56 +0200

    Wind Plant Animation
    * Modified the wind plant's animation to fit the wind activation state.
    * Removed some unused and duplicated animators.
    * Adjusted the level's wind plants to use the new changes.
    * Fixed the jump mushroom animations not being properly played when more
    than one model was used.

--- Assets/Scripts/Scenario/Jump Mushroom/JumpMushroomAnimation.cs
+++ Assets/Scripts/Scenario/Jump Mushroom/JumpMushroomAnimation.cs
@@ -1,24 +1,24 @@
 ﻿using UnityEngine;
 
 /// <summary>
-/// Listener to the mushroom's events that creates plays an animation on the mushroom.
+/// Listener to the mushroom's events that plays an animation on the mushroom.
 /// </summary>
 [RequireComponent(typeof(JumpMushroom))]
 public class JumpMushroomAnimation : MonoBehaviour, JumpMushroomListener {
 
 	/// <summary>
-	/// The effect created when something bounces on it.
+	/// Reference to the affected animators.
 	/// </summary>
-	private Animator _animator;
+	private Animator[] _animators;
 
 	/// <summary>
 	/// The key string to the bounce trigger on the animator.
 	/// </summary>
-	private static readonly string ANIMATOR_TRIGGER_STRING = "Bounce";
+	private static readonly string ANIMATOR_TRIGGER_STRING = "bounce";
 
 	void Awake() {
 		// Retrieves the desired components
-		_animator = GetComponentInChildren<Animator>();
+		_animators = GetComponentsInChildren<Animator>();
 	}
 
 	void Start() {
@@ -27,7 +27,7 @@ public class JumpMushroomAnimation : MonoBehaviour, JumpMushroomListener {
 	}
 
 	public void OnBounce(JumpMushroom mushroom, GameObject bouncingCharacter, Vector3 bounceVelocity, Vector3 collisionPoint, Vector3 collisionNormal) {
-		if (_animator != null)
-			_animator.SetTrigger(ANIMATOR_TRIGGER_STRING);
+		foreach (Animator animator in _animators)
+			animator.SetTrigger(ANIMATOR_TRIGGER_STRING);
 	}
 }
---
+++ Assets/Scripts/Scenario/Wind Tube/WindTubeAnimation.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Listener to the wind tube's events that plays an animation on the plant.
+/// </summary>
+[RequireComponent(typeof(WindTube))]
+public class WindTubeAnimation : MonoBehaviour {
+
+	/// <summary>
+	/// Sets de velocity for the played animations.
+	/// </summary>
+	public float animationSpeed = 1.0f;
+
+	/// <summary>
+	/// Reference to the affected animators.
+	/// </summary>
+	private Animator[] _animators;
+
+	/// <summary>
+	/// Reference to the entity's WindTube component.
+	/// </summary>
+	private WindTube _windTube;
+
+	/// <summary>
+	/// The key string to the active flag on the animator.
+	/// </summary>
+	private static readonly string ANIMATOR_ACTIVE_STRING = "active";
+
+	/// <summary>
+	/// The key string to the animation speed factor on the animator.
+	/// </summary>
+	private static readonly string ANIMATOR_SPEED_STRING = "animation_speed";
+
+	void Awake() {
+		// Retrieves the desired components
+		_animators = GetComponentsInChildren<Animator>();
+		_windTube = GetComponent<WindTube>();
+    }
+
+	void Update() {
+		foreach (Animator animator in _animators) {
+			animator.SetFloat(ANIMATOR_SPEED_STRING, animationSpeed);
+			animator.SetBool(ANIMATOR_ACTIVE_STRING, _windTube.isActiveAndEnabled);
+		}
+	}
+}
