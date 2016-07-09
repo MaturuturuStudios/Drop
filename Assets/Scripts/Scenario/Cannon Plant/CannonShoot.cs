5050ee92684542c26e9ce08bbd0be4771f44a609
Roglerto <roglerto@gmail.com>
2016-07-10 00:04:24 +0200

    canon plant animation time

--- Assets/Scripts/Scenario/Cannon Plant/CannonShoot.cs
+++ Assets/Scripts/Scenario/Cannon Plant/CannonShoot.cs
@@ -7,10 +7,20 @@ using System.Collections.Generic;
 [ExecuteInEditMode]
 public class CannonShoot : ActionPerformer {
     public LaunchCharacter launch;
-	/// <summary>
-	/// List of listeners registered to this component's events.
-	/// </summary>
-	private List<CannonShootListener> _listeners = new List<CannonShootListener>();
+
+    public float AnimationTime=0.0f;
+
+    private float _animationtime;
+
+    private bool _throwing=false;
+
+    private CharacterControllerCustom _ccc;
+
+    private GameObject _character;
+    /// <summary>
+    /// List of listeners registered to this component's events.
+    /// </summary>
+    private List<CannonShootListener> _listeners = new List<CannonShootListener>();
 
 	/// <summary>
 	/// Subscribes a listener to the components's events.
@@ -42,24 +52,42 @@ public class CannonShoot : ActionPerformer {
 	/// Unity's method called each frame.
 	/// </summary>
 	public void Update(){
-		transform.eulerAngles = new Vector3(0, 0, launch.GetAngle()); //this is to face in the direction you are aming
+		//transform.eulerAngles = new Vector3(0, 0, launch.GetAngle()); //this is to face in the direction you are aming
+
+        if (_throwing){
+            Debug.Log(" timer " + _animationtime);
+            _animationtime += Time.deltaTime;
+            ThrowDrop();
+        }
 	}
 
 	protected override bool OnAction(GameObject character)	{
-        CharacterControllerCustom ccc = character.GetComponent<CharacterControllerCustom>();
-
-        ccc.transform.position = this.transform.position;
-        ccc.Stop();
-        Vector3 velocity = launch.GetNeededVelocityVector();
-        ccc.SendFlying(velocity);
-
-		// Notifies the listeners
-		foreach (CannonShootListener listener in character.GetComponents<CannonShootListener>())
-			listener.OnCannonShoot(this, character, velocity);
-		foreach (CannonShootListener listener in _listeners)
-			listener.OnCannonShoot(this, character, velocity);
-
+        _ccc = character.GetComponent<CharacterControllerCustom>();
+        character.SetActive(false);
+        _character = character;
+        _throwing = true;
 		return true;
+    }
+
+    public void ThrowDrop(){
+        
+        if (_animationtime > AnimationTime){
+            _throwing = false;
+            _animationtime = 0.0f;
+            _character.SetActive(true);
+            _ccc.transform.position = launch.pointOrigin.transform.position;
+            _ccc.Stop();
+             Vector3 velocity = launch.GetNeededVelocityVector();
+
+        
+            _ccc.SendFlying(velocity);
+
+            // Notifies the listeners
+            foreach (CannonShootListener listener in _character.GetComponents<CannonShootListener>())
+                listener.OnCannonShoot(this, _character, velocity);
+            foreach (CannonShootListener listener in _listeners)
+                listener.OnCannonShoot(this, _character, velocity);
+        }
     }
 
     public void ChangeAngleCannon(float newAngle) {
