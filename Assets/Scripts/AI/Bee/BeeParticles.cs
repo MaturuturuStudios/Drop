3a9d3595ef90bb5a3a4bb1958ae53339d3b46129
Dorian Hawkmoon <dorian.cadenas@gmail.com>
2016-07-10 22:46:19 +0200

    Added script for the particle in bee

---
+++ Assets/Scripts/AI/Bee/BeeParticles.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(AIBee))]
+public class BeeParticles : MonoBehaviour, EnemyBehaviourListener {
+    /// <summary>
+    /// Particle system of walking effect
+    /// </summary>
+    public GameObject flyingFX;
+
+    /// <summary>
+    /// Check if the particles was set
+    /// </summary>
+    private bool settedFX = true;
+
+    /// <summary>
+	/// Reference to the ant's AI component.
+	/// </summary>
+	private AIBee _ai;
+
+    void Awake() {
+        // Retrieves the desired components
+        _ai = GetComponent<AIBee>();
+    }
+
+    void Start() {
+        // Subscribes itself to the AI events
+        _ai.AddListener(this);
+    }
+
+    public void OnStateAnimationChange(AnimationState previousState, AnimationState actualState) {
+        bool change = false;
+        switch (actualState) {
+            case AnimationState.HIDDE_RECOLECT:
+                if (!settedFX) break;
+                settedFX = false;
+                change = true;
+                break;
+
+            default:
+                if (settedFX) break;
+                settedFX = true;
+                change = true;
+                break;
+                
+        }
+
+        if (change) {
+            foreach (ParticleSystem system in flyingFX.GetComponentsInChildren<ParticleSystem>()) {
+                ParticleSystem.EmissionModule emission = system.emission;
+                emission.enabled = settedFX;
+            }
+        }
+    }
+
+    public void OnBeginChase(AIBase enemy, GameObject chasedObject) {
+    }
+
+    public void OnEndChase(AIBase enemy, GameObject chasedObject) {
+    }
+
+    public void OnAttack(AIBase enemy, GameObject attackedObject, Vector3 velocity) {
+    }
+
+    public void OnBeingScared(AIBase enemy, GameObject scaringObject, int scaringSize) {
+    }
+}
