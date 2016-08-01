422b93d7593d75d691a340351e143784a27214dd
Dorian Hawkmoon <dorian.cadenas@gmail.com>
2016-08-01 18:58:12 +0200

    Added brush selection to main menu

---
+++ Assets/Scripts/UI/Menu/Main Menu/OnSelectMainMenu.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class OnSelectMainMenu : MonoBehaviour, ISelectHandler {
+    /// <summary>
+    /// The main menu script to call
+    /// </summary>
+    public MainMenu mainMenu;
+    
+
+    public void OnSelect(BaseEventData evData) {
+        if (mainMenu == null) return;
+        mainMenu.MoveBrushSelection(this.gameObject);
+    }
+}
