3ec7d0266c1082b9d726d1c6515e70c2924d3d7e
Dorian Hawkmoon <dorian.cadenas@gmail.com>
2016-08-14 19:28:34 +0200

    Added script editor for player prefs

---
+++ Assets/Editor/PlayerPrefs/PlayerPrefsEditorUtility.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+/// <summary>
+/// The MIT License (MIT)
+/// Copyright (c) 2015 Kuutti Entertainment Ltd
+/// </summary>
+public class PlayerPrefsEditorUtility {
+
+    [MenuItem("PlayerPrefs/Delete All")]
+    static void DeletePlayerPrefs() {
+        PlayerPrefs.DeleteAll();
+        Debug.Log("All PlayerPrefs deleted");
+    }
+
+    [MenuItem("PlayerPrefs/Save All")]
+    static void SavePlayerPrefs() {
+        PlayerPrefs.Save();
+        Debug.Log("PlayerPrefs saved");
+    }
+}
