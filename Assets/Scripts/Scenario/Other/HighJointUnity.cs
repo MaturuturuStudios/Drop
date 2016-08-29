ace37d7e16791d0b1b519685a5e4ec3bea7aa73a
Roglerto <roglerto@gmail.com>
2016-08-29 19:36:46 +0200

    workign with hight

---
+++ Assets/Scripts/Scenario/Other/HighJointUnity.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HighJointUnity : MonoBehaviour {
+
+    public float rigbodyMassIncrease=10;
+
+    public float rigbodyMass=100;
+
+    private float _rigbody;
+
+    private bool _onEnter = false;
+    private bool _onExit = false;
+
+    // Use this for initialization
+    void Start () {
+        rigbodyMass = transform.parent.GetComponent<Rigidbody>().mass;
+        _rigbody = rigbodyMassIncrease;
+	}
+
+
+    public void OnTriggerEnter(Collider other)
+    {
+        CharacterControllerCustom cccp = other.gameObject.GetComponent<CharacterControllerCustom>();
+
+        if (cccp != null)
+        {
+            if (cccp.GetTotalMass() > 2) {                
+                HingeJoint hinge = transform.parent.GetComponent<HighJoint>(); ;
+                JointSpring hingeSpring = hinge.spring;
+            }
+            Debug.Log(" MASAAAAAAAA " + transform.parent.GetComponent<Rigidbody>().mass);
+        }
+
+    }
+
+    public void OnTriggerExit(Collider other)
+    {
+        //transform.eulerAngles = _initialPosition;
+        CharacterControllerCustomPlayer cccp = other.gameObject.GetComponent<CharacterControllerCustomPlayer>();
+
+        if (cccp != null)
+        {
+            transform.parent.GetComponent<Rigidbody>().mass = rigbodyMass;
+        }
+    }
+}
