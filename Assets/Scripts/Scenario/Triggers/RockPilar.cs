59865dc1d07b69125c3c4d2b39a0fe31834bd3d4
Ignacio Astorga Coto <iacfromearth@gmail.com>
2016-09-17 20:17:26 +0200

    Rock Pilar Fix
    * Created a new prefab for the rock pilar, replacing the previous one.
    * The rock pilar will now make the sound effect fade in and out instead
    of simply muting it.
    * Replaced the rock pilar sound with an stronger one.
    * The jelly buttons and the pilars on the main level and the second part
    of the tutorial are now instances of their respective prefabs instead of
    standalone objects.

--- Assets/Scripts/Scenario/Triggers/RockPilar.cs
+++ Assets/Scripts/Scenario/Triggers/RockPilar.cs
@@ -19,6 +19,16 @@ public class RockPilar : MonoBehaviour, FollowPathListener {
 	/// </summary>
 	public GameObject topParticleEffect;
 
+    /// <summary>
+    /// Time for the sound end fading in.
+    /// </summary>
+    public float soundFadeInTime = 0.05f;
+
+    /// <summary>
+    /// Time for the sound end fading out.
+    /// </summary>
+    public float soundFadeOutTime = 0.5f;
+
 	/// <summary>
 	/// Reference to this object's AudioSource component.
 	/// </summary>
@@ -28,6 +38,11 @@ public class RockPilar : MonoBehaviour, FollowPathListener {
 	/// Reference to this object's Animator component.
 	/// </summary>
 	private Animator _animator;
+
+    /// <summary>
+    /// Target volume for the sound playing.
+    /// </summary>
+    private float _targetVolume;
 
 	void Start() {
 		// Retrieves the desired components
@@ -40,15 +55,41 @@ public class RockPilar : MonoBehaviour, FollowPathListener {
 		// Stops the particle effects
 		groundParticleEffect.DisableParticleSystems();
 		topParticleEffect.DisableParticleSystems();
+
+        // Sets the sound volume
+        _targetVolume = 0;
+        _audioSource.volume = _targetVolume;
 	}
+
+    void Update() {
+        // Sets the sound volume to the target one
+        if (_audioSource.volume < _targetVolume) {
+            // Increases the volume
+            if (soundFadeInTime == 0)
+                // Instantly
+                _audioSource.volume = _targetVolume;
+            else
+                // Over soundFadeInTime seconds
+                _audioSource.volume = Mathf.Min(_targetVolume, _audioSource.volume + Time.deltaTime / soundFadeInTime);
+        }
+        else {
+            // Decreases the volume
+            if (soundFadeOutTime == 0)
+                // Instantly
+                _audioSource.volume = _targetVolume;
+            else
+                // Over soundFadeOutTime seconds
+                _audioSource.volume = Mathf.Max(_targetVolume, _audioSource.volume - Time.deltaTime / soundFadeOutTime);
+        }
+    }
 
 	public void OnKeepMoving(Vector3 position, Vector3 destination, Vector3 velocity) {
 		// Do nothing
 	}
 
 	public void OnStartMoving(Vector3 position, Vector3 destination, Vector3 velocity) {
-		// Plays the sound
-		_audioSource.Play();
+        // Fades in the sound
+        _targetVolume = 1;
 
 		// Plays the particle effects
 		groundParticleEffect.EnableParticleSystems();
@@ -59,11 +100,11 @@ public class RockPilar : MonoBehaviour, FollowPathListener {
 	}
 
 	public void OnStopMoving(Vector3 position) {
-		// Stops the sound
-		_audioSource.Stop();
+        // Fades out the sound
+        _targetVolume = 0;
 
-		// Plays the particle effects
-		groundParticleEffect.DisableParticleSystems();
+        // Plays the particle effects
+        groundParticleEffect.DisableParticleSystems();
 		topParticleEffect.DisableParticleSystems();
 
 		// Plays the animation
