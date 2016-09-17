69c727cea7c4858768c87d7382c7f70455c61163
Ignacio Astorga Coto <iacfromearth@gmail.com>
2016-09-17 20:05:54 +0200

    Jelly Button
    * Replaced the previous button prefab with a new "Jelly Button" one.
    * Added animations to the jelly button when it is pressed or released.
    * Added a sound effect to the jelly button when it is pressed or
    released.
    * Added a particle effect to the jelly button when it is pressed.

---
+++ Assets/Scripts/Scenario/Triggers/ButtonEffects.cs
@@ -0,0 +1,75 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Contains methods to create the effects of the buttos.
+/// </summary>
+public class ButtonEffects : MonoBehaviour {
+
+    /// <summary>
+    /// Key for the animator field.
+    /// </summary>
+    public static readonly string ANIMATOR_KEY = "pressed";
+
+    /// <summary>
+    /// Reference to the animator.
+    /// </summary>
+    public Animator animator;
+
+    /// <summary>
+    /// Sound played when the button is pressed.
+    /// </summary>
+    public AudioClip pressSound;
+
+    /// <summary>
+    /// Sound played when the button is released.
+    /// </summary>
+    public AudioClip releaseSound;
+
+    /// <summary>
+    /// Particle effect played when the button is pressed.
+    /// </summary>
+    public EffectInformation particleEffect;
+
+    /// <summary>
+    ///  Reference to this entity's AudioSource component.
+    /// </summary>
+    private AudioSource _audioSource;
+
+    /// <summary>
+    ///  Reference to this entity's Transform component.
+    /// </summary>
+    private Transform _transform;
+
+    void Start() {
+        // Retrieves the desired components
+        _audioSource = GetComponent<AudioSource>();
+        _transform = transform;
+    }
+
+    /// <summary>
+    /// Plays the press effects.
+    /// </summary>
+    public void Press() {
+        // Plays the animation
+        animator.SetBool(ANIMATOR_KEY, true);
+
+        // Plays the sound
+        _audioSource.clip = pressSound;
+        _audioSource.Play();
+
+        // Creates the particle effect
+        particleEffect.PlayEffect(_transform.position, _transform.rotation);
+    }
+
+    /// <summary>
+    /// Plays the release effects.
+    /// </summary>
+    public void Release() {
+        // Plays the animation
+        animator.SetBool(ANIMATOR_KEY, false);
+
+        // Plays the sound
+        _audioSource.clip = releaseSound;
+        _audioSource.Play();
+    }
+}
