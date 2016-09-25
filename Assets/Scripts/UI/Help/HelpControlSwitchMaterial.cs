30885f9c2e2566bbd37524092265c65605aea3bd
Ignacio Astorga Coto <iacfromearth@gmail.com>
2016-09-25 18:07:39 +0200

    Help Control Switch
    * Added a new script whch allows an object to alternate its material
    when a controller is attached.
    * Added a version for UI images as well, which changes their sprite.

---
+++ Assets/Scripts/UI/Help/HelpControlSwitchMaterial.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Renderer))]
+public class HelpControlSwitchMaterial : MonoBehaviour {
+
+	public Material keyboardMaterial;
+	public Material controllerMaterial;
+
+	private Renderer _renderer;
+
+	void Start() {
+		_renderer = GetComponent<Renderer>();
+	}
+
+	void Update() {
+		if (Input.GetJoystickNames().Length == 0)
+			_renderer.material = keyboardMaterial;
+		else
+			_renderer.material = controllerMaterial;
+	}
+}
---
+++ Assets/Scripts/UI/Help/HelpControlSwitchUI.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Image))]
+public class HelpControlSwitchUI : MonoBehaviour {
+
+	public Sprite keyboardImage;
+	public Sprite controllerImage;
+
+	private Image _renderer;
+
+	void Start() {
+		_renderer = GetComponent<Image>();
+	}
+
+	void Update() {
+		if (Input.GetJoystickNames().Length == 0)
+			_renderer.sprite = keyboardImage;
+		else
+			_renderer.sprite = controllerImage;
+	}
+}
