cbb69b9586d2e99130a888bd927a68d70e858d7b
Toni Lopez <alopezs1987@gmail.com>
2016-09-26 02:13:34 +0200

    Menu movie test
    * Menu movie test

---
+++ Assets/Scripts/UI/Menu/PlayVideo.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayVideo : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+        // Get the movie texture
+        MovieTexture introMovie = GetComponentInChildren<RawImage>().texture as MovieTexture;
+
+        // Set it looping
+        introMovie.loop = true;
+
+        // Play the video
+        introMovie.Play();
+    }
+}
