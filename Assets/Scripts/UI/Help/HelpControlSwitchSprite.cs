7dea883175c6b44e41c32ca7a58a32d819eb575a
Ignacio Astorga Coto <iacfromearth@gmail.com>
2016-10-04 22:34:48 +0200

    Irrigate Help Key
    * The button shown on the irrigate help will now switch between button
    or key if a controller is connected or not respectively.

---
+++ Assets/Scripts/UI/Help/HelpControlSwitchSprite.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(SpriteRenderer))]
+public class HelpControlSwitchSprite : MonoBehaviour {
+
+	public Sprite keyboardSprite;
+	public Sprite controllerSprite;
+
+	private SpriteRenderer _renderer;
+
+	void Start() {
+		_renderer = GetComponent<SpriteRenderer>();
+	}
+
+	void Update() {
+		if (Input.GetJoystickNames().Length == 0)
+			_renderer.sprite = keyboardSprite;
+		else
+			_renderer.sprite = controllerSprite;
+	}
+}
