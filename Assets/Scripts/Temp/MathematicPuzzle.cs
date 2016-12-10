b49c75a597ce6937e54e0522480fe40142c85f60
Dorian Hawkmoon <dorian.cadenas@gmail.com>
2016-12-11 00:00:37 +0100

    Crazy puzzle

---
+++ Assets/Scripts/Temp/MathematicPuzzle.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MathematicPuzzle : MonoBehaviour {
+	/// <summary>
+	/// The previous value.
+	/// </summary>
+	private int previousValue=1;
+	/// <summary>
+	/// Reference to the original transform
+	/// </summary>
+	private Vector3 originalPosition;
+	/// <summary>
+	/// The max value to clamp
+	/// </summary>
+	public int maxValue=6;
+	/// <summary>
+	/// The minimum value to clamp
+	/// </summary>
+	public int minValue=-3;
+
+	public void Start(){
+		originalPosition = this.transform.position;
+	}
+
+	public void Add(int value){
+		previousValue += value;
+		Vector3 position = originalPosition;
+		position.y += Mathf.Clamp(previousValue,minValue,maxValue);
+		this.transform.position = position;
+	}
+
+	public void Multiply(int value){
+		previousValue *= value;
+		Vector3 position = originalPosition;
+		position.y += Mathf.Clamp(previousValue,minValue,maxValue);
+		this.transform.position = position;
+	}
+
+	public void Reset(){
+		previousValue = 1;
+		this.transform.position = originalPosition;
+	}
+}
